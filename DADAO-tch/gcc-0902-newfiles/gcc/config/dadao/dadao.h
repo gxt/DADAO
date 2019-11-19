12a23985656500b42805a0f7c72064a5b9d6ad1d
Guan Xuetao <gxt@pku.edu.cn>
2019-11-19 10:09:15 +0800

    TCH-gcc: add FIRST_PSEUDO_REGISTER and REGISTER_NAMES
    Signed-off-by: Guan Xuetao <gxt@pku.edu.cn>

--- DADAO-tch/gcc-0902-newfiles/gcc/config/dadao/dadao.h
+++ DADAO-tch/gcc-0902-newfiles/gcc/config/dadao/dadao.h
@@ -5,3 +5,30 @@
 #define WORDS_BIG_ENDIAN	1
 
 #define UNITS_PER_WORD		8
+
+/* register desciption */
+#define FIRST_PSEUDO_REGISTER (64 * 2)
+
+#define REGISTER_NAMES {						\
+  /* reg-data */							\
+  "zero", "rd01", "rd02", "rd03", "rd04", "rd05", "rd06", "rd07",	\
+  "rd08", "rd09", "rd10", "rd11", "rd12", "rd13", "rd14", "rd15",	\
+  "rd16", "rd17", "rd18", "rd19", "rd20", "rd21", "rd22", "rd23",	\
+  "rd24", "rd25", "rd26", "rd27", "rd28", "rd29", "rd30", "rd31",	\
+  "rd32", "rd33", "rd34", "rd35", "rd36", "rd37", "rd38", "rd39",	\
+  "rd40", "rd41", "rd42", "rd43", "rd44", "rd45", "rd46", "rd47",	\
+  "rd48", "rd49", "rd50", "rd51", "rd52", "rd53", "rd54", "rd55",	\
+  "rd56", "rd57", "rd58", "rd59", "rd60", "rd61", "rd62", "rd63",	\
+  /* reg-pointer */							\
+  "null", "rp01", "rp02", "rp03", "rp04", "rp05", "rp06", "rp07",	\
+  "rp08", "rp09", "rp10", "rp11", "rp12", "rp13", "rp14", "rp15",	\
+  "rp16", "rp17", "rp18", "rp19", "rp20", "rp21", "rp22", "rp23",	\
+  "rp24", "rp25", "rp26", "rp27", "rp28", "rp29", "rp30", "rp31",	\
+  "rp32", "rp33", "rp34", "rp35", "rp36", "rp37", "rp38", "rp39",	\
+  "rp40", "rp41", "rp42", "rp43", "rp44", "rp45", "rp46", "rp47",	\
+  "rp48", "rp49", "rp50", "rp51", "rp52", "rp53", "rp54", "rp55",	\
+  "rp56", "rp57", "rp58", "rp59", "rp60", "rp61", "rp62", "rp63",	\
+  /* pseudo registers */						\
+  "ra00" /* SHOULD NEVER BE USED DIRECTLY */				\
+}
+ 
