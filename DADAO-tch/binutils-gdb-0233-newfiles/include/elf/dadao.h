821765e932aa6cd7960542aa5df14b90c10b0920
Guan Xuetao <gxt@pku.edu.cn>
2020-04-06 09:03:02 +0000

    TCH-bin: update include/eld/dadao.h
    Signed-off-by: Guan Xuetao <gxt@pku.edu.cn>

--- DADAO-tch/binutils-gdb-0233-newfiles/include/elf/dadao.h
+++ DADAO-tch/binutils-gdb-0233-newfiles/include/elf/dadao.h
@@ -6,57 +6,133 @@
  *   2019:
  *	Guan Xuetao <gxt@pku.edu.cn>
  */
-#ifndef _ELF_DADAO_H
-#define _ELF_DADAO_H
+#ifndef ELF_DADAO_H
+#define ELF_DADAO_H
 
-#include "bfd.h"
-#include "elf/common.h"
 #include "elf/reloc-macros.h"
-#include "bfd_stdint.h"
 
-#ifdef __cplusplus
-extern "C"
-{
-#endif
-
-#define EF_DADAO_MACH(ef_dadao)        (((ef_dadao) >> 8) & 0xFFFF)
-#define EF_DADAO_SET_MACH(dadao_fam)   (((dadao_fam) & 0xFFFF) << 8)
-
-#define E_DADAO_MACH_WUXING	0x0100
-#define E_DADAO_MACH_WUQING	0x0200
-#define E_DADAO_MACH_WUMING	0x0400
-
-/* DADAO Section types
-   INITREG - A generic register initialisation section
-   UDEBUG - Legacy-style debug data section.  */
-#define SHT_DADAO_INITREG		(SHT_LOPROC + 2)
-#define SHT_DADAO_UDEBUG		SHT_LOUSER
-
-/* DADAO SECTION flags
-     ELF-64 sh_flags is 64-bit, but there is no info on what the upper 32 bits
-     are expected to be used for, it is not marked reserved either.
-     We'll use them for DADAO-specific flags since we don't use ELF-32.
-
-   INIT - Sections that are loaded and executed before the final text
-	  microcode.  Non-code INIT sections are loaded first, then other
-	  memory secions, then INIT2 sections, then INIT-code sections.
-   INIT2 - Sections that are loaded before INIT-code sections, used for
-	   transient configuration before executing INIT-code section
-	   microcode. */
-
-#define SHF_DADAO_INIT			0x80000000
-#define SHF_DADAO_INIT2			0x40000000
-
-/* Relocations.  */
+/* Relocations.  See the reloc table in bfd/elf64-dadao.c for details.  */
 START_RELOC_NUMBERS (elf_dadao_reloc_type)
-    RELOC_NUMBER (R_DADAO_NOTYPE, 0)
-    RELOC_NUMBER (R_DADAO_IMM12, 1)
-    RELOC_NUMBER (R_DADAO_IMM18, 2)
-    RELOC_NUMBER (R_DADAO_IMM24, 3)
-END_RELOC_NUMBERS (R_DADAO_MAX)
+  RELOC_NUMBER (R_DADAO_NONE, 0)
 
-#ifdef __cplusplus
-}
+  /* Standard absolute relocations.  */
+  RELOC_NUMBER (R_DADAO_8, 1)
+  RELOC_NUMBER (R_DADAO_16, 2)
+  RELOC_NUMBER (R_DADAO_24, 3)
+  RELOC_NUMBER (R_DADAO_32, 4)
+  RELOC_NUMBER (R_DADAO_64, 5)
+
+  /* Standard relative relocations.  */
+  RELOC_NUMBER (R_DADAO_PC_8, 6)
+  RELOC_NUMBER (R_DADAO_PC_16, 7)
+  RELOC_NUMBER (R_DADAO_PC_24, 8)
+  RELOC_NUMBER (R_DADAO_PC_32, 9)
+  RELOC_NUMBER (R_DADAO_PC_64, 10)
+
+  /* GNU extensions for C++ vtables.  */
+  RELOC_NUMBER (R_DADAO_GNU_VTINHERIT, 11)
+  RELOC_NUMBER (R_DADAO_GNU_VTENTRY, 12)
+
+  /* A GETA instruction.  */
+  RELOC_NUMBER (R_DADAO_GETA, 13)
+  RELOC_NUMBER (R_DADAO_GETA_1, 14)
+  RELOC_NUMBER (R_DADAO_GETA_2, 15)
+  RELOC_NUMBER (R_DADAO_GETA_3, 16)
+
+  /* A conditional branch instruction.  */
+  RELOC_NUMBER (R_DADAO_CBRANCH, 17)
+  RELOC_NUMBER (R_DADAO_CBRANCH_J, 18)
+  RELOC_NUMBER (R_DADAO_CBRANCH_1, 19)
+  RELOC_NUMBER (R_DADAO_CBRANCH_2, 20)
+  RELOC_NUMBER (R_DADAO_CBRANCH_3, 21)
+
+  /* A PUSHJ instruction.  */
+  RELOC_NUMBER (R_DADAO_PUSHJ, 22)
+  RELOC_NUMBER (R_DADAO_PUSHJ_1, 23)
+  RELOC_NUMBER (R_DADAO_PUSHJ_2, 24)
+  RELOC_NUMBER (R_DADAO_PUSHJ_3, 25)
+
+  /* A JMP instruction.  */
+  RELOC_NUMBER (R_DADAO_JMP, 26)
+  RELOC_NUMBER (R_DADAO_JMP_1, 27)
+  RELOC_NUMBER (R_DADAO_JMP_2, 28)
+  RELOC_NUMBER (R_DADAO_JMP_3, 29)
+
+  /* A relative address such as in a GETA or a branch.  */
+  RELOC_NUMBER (R_DADAO_ADDR19, 30)
+
+  /* A relative address such as in a JMP (only).  */
+  RELOC_NUMBER (R_DADAO_ADDR27, 31)
+
+  /* A general register or a number 0..255.  */
+  RELOC_NUMBER (R_DADAO_REG_OR_BYTE, 32)
+
+  /* A general register. */
+  RELOC_NUMBER (R_DADAO_REG, 33)
+
+  /* A global register and an offset, the global register (allocated at
+     link time) contents plus the offset made equivalent to the relocation
+     expression at link time.  The relocation must point at the Y field of
+     an instruction.  */
+  RELOC_NUMBER (R_DADAO_BASE_PLUS_OFFSET, 34)
+
+  /* A LOCAL assertion.  */
+  RELOC_NUMBER (R_DADAO_LOCAL, 35)
+
+  /* A PUSHJ instruction, generating a stub if it does not reach.  */
+  RELOC_NUMBER (R_DADAO_PUSHJ_STUBBABLE, 36)
+END_RELOC_NUMBERS (R_DADAO_max)
+
+
+/* Section Attributes.  */
+/* A section containing necessary information for relaxation.  */
+#define SHF_DADAO_CANRELAX	0x80000000
+
+/* Symbol attributes.  */
+/* A symbol with this section-index is a register.  */
+#define SHN_REGISTER	SHN_LOPROC
+
+/* This section holds contents for each initialized register, at VMA
+   regno*8.  A symbol relative to this section will be transformed to an
+   absolute symbol with the value corresponding to the register number at
+   final link time.  A symbol with a value outside the inclusive range
+   32*8 .. 254*8 is an error.  It is highly recommended to only use an
+   upper bound of 253*8 or lower as specified in the (currently
+   unspecified) ABI.  */
+#define DADAO_REG_CONTENTS_SECTION_NAME ".DADAO.reg_contents"
+
+/* At link time, a section by this name is created, expected to be
+   included in DADAO_REG_CONTENTS_SECTION_NAME in the output.  */
+#define DADAO_LD_ALLOCATED_REG_CONTENTS_SECTION_NAME \
+ ".DADAO.reg_contents.linker_allocated"
+
+/* This is a faked section holding symbols with SHN_REGISTER.  Don't
+   confuse it with DADAO_REG_CONTENTS_SECTION_NAME; this one has no
+   contents, just values.  It is an error for a value in this section to
+   be outside the range 32..255 and it must never become an actual section
+   in an object file.  */
+#define DADAO_REG_SECTION_NAME "*REG*"
+
+/* Appended with a number N=0..65535, this is a representation of the
+   dadaoal "BSPEC N" ... "ESPEC" directive pair; the contents go into an
+   ELF section by name ".DADAO.spec_data.N".  */
+#define DADAO_OTHER_SPEC_SECTION_PREFIX ".DADAO.spec_data."
+
+/* A section SECNAME is noted to start at "__.DADAO.start.SECNAME" by the
+   presence of this symbol.  Currently only implemented for ".text"
+   through the symbol "__.DADAO.start..text".  */
+#define DADAO_LOC_SECTION_START_SYMBOL_PREFIX "__.DADAO.start."
+
+/* This symbol is always a function.  */
+#define DADAO_START_SYMBOL_NAME "Main"
+
+#ifdef BFD_ARCH_SIZE
+extern bfd_boolean _bfd_dadao_before_linker_allocation
+  (bfd *, struct bfd_link_info *);
+extern bfd_boolean _bfd_dadao_after_linker_allocation
+  (bfd *, struct bfd_link_info *);
+extern bfd_boolean _bfd_dadao_check_all_relocs
+  (bfd *, struct bfd_link_info *);
 #endif
 
-#endif /* _ELF_DADAO_H */
+#endif /* ELF_DADAO_H */
