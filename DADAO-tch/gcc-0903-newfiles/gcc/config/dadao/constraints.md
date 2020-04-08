1d778e8fa75b140201f194543d74880635058565
Guan Xuetao <gxt@pku.edu.cn>
2020-04-08 11:08:02 +0000

    TCH-gcc: add gcc-0903-newfiles/gcc/config/dadao
    Signed-off-by: Guan Xuetao <gxt@pku.edu.cn>

---
+++ DADAO-tch/gcc-0903-newfiles/gcc/config/dadao/constraints.md
@@ -0,0 +1,104 @@
+;; DADAO constraints
+;; Copyright (C) 2020-2033 by Guan Xuetao (AT) Peking Univ.
+;;
+;; Contributed by:
+;;   2020:
+;;	Guan Xuetao <gxt@pku.edu.cn>
+
+(define_register_constraint "x" "SYSTEM_REGS"
+  "@internal")
+
+(define_register_constraint "y" "REMAINDER_REG"
+  "@internal")
+
+(define_register_constraint "z" "HIMULT_REG"
+  "@internal")
+
+(define_constraint "I"
+  "A 8-bit unsigned integer"
+  (and (match_code "const_int")
+       (match_test "IN_RANGE (ival, 0, 255)")))
+
+(define_constraint "J"
+  "A 16-bit unsigned integer."
+  (and (match_code "const_int")
+       (match_test "IN_RANGE (ival, 0, 65535)")))
+
+(define_constraint "K"
+  "An integer between -255 and 0."
+  (and (match_code "const_int")
+       (match_test "IN_RANGE (ival, -255, 0)")))
+
+(define_constraint "L"
+  "@internal"
+  (and (match_code "const_int")
+       (match_test "dadao_shiftable_wyde_value (ival)")))
+
+(define_constraint "M"
+  "The value 0."
+  (and (match_code "const_int")
+       (match_test "ival == 0")))
+
+(define_constraint "N"
+  "@internal"
+  (and (match_code "const_int")
+       (match_test "dadao_shiftable_wyde_value (~ival)")))
+
+(define_constraint "O"
+  "The value 3, 5, 9, or 17."
+  (and (match_code "const_int")
+       (ior (match_test "ival == 3")
+	    (match_test "ival == 5")
+	    (match_test "ival == 9")
+	    (match_test "ival == 17"))))
+
+;; FIXME: M (or G) is redundant.
+
+(define_constraint "G"
+  "Floating-point zero."
+  (and (match_code "const_double")
+       (match_test "op == CONST0_RTX (mode)")))
+
+;; R asks whether x is to be loaded with GETA or something else.  Right
+;; now, only a SYMBOL_REF and LABEL_REF can fit for
+;; TARGET_BASE_ADDRESSES.
+;;
+;; Only constant symbolic addresses apply.  With TARGET_BASE_ADDRESSES,
+;; we just allow straight LABEL_REF or SYMBOL_REFs with SYMBOL_REF_FLAG
+;; set right now; only function addresses and code labels.  If we change
+;; to let SYMBOL_REF_FLAG be set on other symbols, we have to check
+;; inside CONST expressions.  When TARGET_BASE_ADDRESSES is not in
+;; effect, a "raw" constant check together with dadao_constant_address_p
+;; is all that's needed; we want all constant addresses to be loaded
+;; with GETA then.
+
+(define_constraint "R"
+  "@internal"
+  (and (not (match_code "const_int,const_double"))
+       (match_test "dadao_constant_address_p (op)")
+       (ior (match_test "!TARGET_BASE_ADDRESSES")
+	    (match_code "label_ref")
+	    (and (match_code "symbol_ref")
+		 (match_test "SYMBOL_REF_FLAG (op)")))))
+
+;; FIXME: L (or S) is redundant.
+
+(define_constraint "S"
+  "@internal"
+  (and (match_code "const_int,const_double")
+       (match_test "dadao_shiftable_wyde_value (dadao_intval (op))")))
+
+;; FIXME: N (or T) is redundant.
+
+(define_constraint "T"
+  "@internal"
+  (and (match_code "const_int,const_double")
+       (match_test "dadao_shiftable_wyde_value (~dadao_intval (op))")))
+
+(define_address_constraint "U"
+  "@internal"
+  (match_operand 0 "dadao_address_operand"))
+
+(define_constraint "Yf"
+  "@internal"
+  (match_operand 0 "frame_pointer_operand"))
---
+++ DADAO-tch/gcc-0903-newfiles/gcc/config/dadao/dadao-protos.h
@@ -0,0 +1,68 @@
+/*
+ * Prototypes for exported functions defined in dadao.c
+ * Copyright (C) 2020-2033 Guan Xuetao (AT) Peking Univ.
+ * Contributed by Guan Xuetao <gxt@pku.edu.cn>
+ */
+
+extern void dadao_init_expanders (void);
+extern int dadao_eh_return_data_regno (int);
+extern int dadao_initial_elimination_offset (int, int);
+extern int dadao_function_arg_regno_p (int, int);
+extern void dadao_function_profiler (FILE *, int);
+extern int dadao_reversible_cc_mode (machine_mode);
+extern const char *dadao_text_section_asm_op (void);
+extern const char *dadao_data_section_asm_op (void);
+extern void dadao_output_quoted_string (FILE *, const char *, int);
+extern void dadao_asm_output_ascii (FILE *, const char *, int);
+extern void dadao_asm_output_label (FILE *, const char *);
+extern void dadao_asm_output_internal_label (FILE *, const char *);
+extern void dadao_asm_weaken_label (FILE *, const char *);
+extern void dadao_asm_output_labelref (FILE *, const char *);
+extern void dadao_asm_output_def (FILE *, const char *, const char *);
+extern void dadao_asm_output_reg_push (FILE *, int);
+extern void dadao_asm_output_reg_pop (FILE *, int);
+extern void dadao_asm_output_skip (FILE *, int);
+extern void dadao_asm_output_align (FILE *, int);
+extern int64_t dadao_intval (const_rtx);
+extern int dadao_shiftable_wyde_value (uint64_t);
+extern void dadao_output_register_setting (FILE *, int, int64_t, int);
+extern int dadao_opposite_regno (int, int);
+extern int dadao_local_regno (int);
+extern unsigned dadao_dbx_register_number (unsigned);
+extern int dadao_use_simple_return (void);
+extern void dadao_make_decl_one_only (tree);
+extern int dadao_data_alignment (tree, int);
+extern unsigned dadao_local_alignment (tree, unsigned);
+extern void dadao_asm_output_pool_prologue (FILE *, const char *, tree, int);
+extern void dadao_asm_output_aligned_common (FILE *, const char *, int, int);
+extern void dadao_asm_output_aligned_local (FILE *, const char *, int, int);
+extern void dadao_asm_declare_register_global
+  (FILE *, tree, int, const char *);
+extern void dadao_asm_output_addr_diff_elt (FILE *, rtx, int, int);
+extern void dadao_asm_output_addr_vec_elt (FILE *, int);
+extern enum reg_class dadao_secondary_reload_class
+  (enum reg_class, machine_mode, rtx, int);
+extern rtx dadao_dynamic_chain_address (rtx);
+extern rtx dadao_return_addr_rtx (int, rtx);
+extern rtx dadao_eh_return_stackadj_rtx (void);
+extern rtx dadao_eh_return_handler_rtx (void);
+extern int dadao_constant_address_p (rtx);
+extern void dadao_expand_prologue (void);
+extern void dadao_expand_epilogue (void);
+extern rtx dadao_get_hard_reg_initial_val (machine_mode, int);
+extern int dadao_asm_preferred_eh_data_format (int, int);
+extern void dadao_setup_frame_addresses (void);
+
+#ifdef RTX_CODE
+/* Needs to be ifdef:d for sake of enum rtx_code.  */
+extern machine_mode dadao_select_cc_mode (enum rtx_code, rtx, rtx);
+extern void dadao_canonicalize_comparison (enum rtx_code *, rtx *, rtx *);
+extern rtx dadao_gen_compare_reg (enum rtx_code, rtx, rtx);
+#endif
+
+/*
+ * Local variables:
+ * eval: (c-set-style "gnu")
+ * indent-tabs-mode: t
+ * End:
+ */
