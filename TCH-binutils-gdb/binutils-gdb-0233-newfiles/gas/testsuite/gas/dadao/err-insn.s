94736cf0f3404fb207ed8b711e2c4adc4bb1a0d5
Guan Xuetao <gxt@pku.edu.cn>
2020-05-06 11:20:55 +0000

    TCH-bin: gnu syntax gas/testsuite/gas/dadao/err-insn.s
    Signed-off-by: Guan Xuetao <gxt@pku.edu.cn>

--- TCH-binutils-gdb/binutils-gdb-0233-newfiles/gas/testsuite/gas/dadao/err-insn.s
+++ TCH-binutils-gdb/binutils-gdb-0233-newfiles/gas/testsuite/gas/dadao/err-insn.s
@@ -1,4 +1,4 @@
-% { dg-do assemble { target dadao-*-* } }
-Main SWYM 0,0,0
- FLOT $112,$223,$41 % { dg-error "invalid operands" "Y field of FLOT 1" }
- FLOT $112,$223,141 % { dg-error "invalid operands" "Y field of FLOT 2" }
+# { dg-do assemble { target dadao-*-* } }
+Main:
+ FLOT $112,$223,$41 ;# { dg-error "invalid operands" "Y field of FLOT 1" }
+ FLOT $112,$223,141 ;# { dg-error "invalid operands" "Y field of FLOT 2" }
