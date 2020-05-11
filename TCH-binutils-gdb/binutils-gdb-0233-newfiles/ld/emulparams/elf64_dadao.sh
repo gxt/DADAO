633f96518de222a4a61d5f5a99059bc7ff7ee85d
Guan Xuetao <gxt@pku.edu.cn>
2020-05-11 07:18:17 +0000

    TCH-bin: change entry name to _start
    Signed-off-by: Guan Xuetao <gxt@pku.edu.cn>

--- TCH-binutils-gdb/binutils-gdb-0233-newfiles/ld/emulparams/elf64_dadao.sh
+++ TCH-binutils-gdb/binutils-gdb-0233-newfiles/ld/emulparams/elf64_dadao.sh
@@ -4,7 +4,7 @@ ELFSIZE=64
 SCRIPT_NAME=elf
 OUTPUT_FORMAT="elf64-dadao"
 NO_REL_RELOCS=yes
-ENTRY=_start.
+ENTRY=_start
 
 # Default to 0
 TEXT_START_ADDR='DEFINED (__.DADAO.start..text) ? __.DADAO.start..text : 0x400000'
@@ -18,31 +18,6 @@ ARCH=dadao
 MACHINE=
 COMPILE_IN=yes
 EXTRA_EM_FILE=dadaoelf
-
-# The existence of a symbol __start (or _start) should overrule Main, so
-# it can be a user symbol without the associated dadaoal magic.  We
-# also want to provide Main as a synonym for _start, if Main wasn't
-# defined but is referred to, and _start was defined.
-#
-# The reason we use a symbol "_start." as a mediator is to avoid
-# causing ld to force the type of _start to object rather than no
-# type, which confuses disassembly; we also make it alphanumerically
-# a successor of _start for similar reasons.  Perhaps it's a linker
-# bug that linker-defined symbols set the symbol-type.
-#
-# Note that we smuggle this into OTHER_TEXT_SECTIONS (at the end
-# of .text) rather than TEXT_START_SYMBOLS.  This is necessary, as
-# DEFINED wouldn't find the symbol if it was at the top; presumably
-# before the definition, if the definition is not in the first file.
-# FIXME: Arguably a linker bug.
-# Only do this for a final link, or else we'll mess up e.g. error
-# messages.
-OTHER_TEXT_SECTIONS="
-${RELOCATING+
- _start. = (DEFINED (_start) ? _start
-	    : (DEFINED (Main) ? Main : (DEFINED (.text) ? .text : 0)));
- PROVIDE (Main = DEFINED (Main) ? Main : (DEFINED (_start) ? _start : _start.));
-}"
 
 OTHER_SECTIONS='
  .DADAO.reg_contents :
