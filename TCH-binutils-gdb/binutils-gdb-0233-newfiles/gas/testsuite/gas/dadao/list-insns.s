b3f5614a11209e841c43c29efbb42fcadc430e3c
Guan Xuetao <gxt@pku.edu.cn>
2020-05-13 00:12:49 +0000

    TCH-bin: add mul/mulu/div/divu testsuite
    Signed-off-by: Guan Xuetao <gxt@pku.edu.cn>

--- TCH-binutils-gdb/binutils-gdb-0233-newfiles/gas/testsuite/gas/dadao/list-insns.s
+++ TCH-binutils-gdb/binutils-gdb-0233-newfiles/gas/testsuite/gas/dadao/list-insns.s
@@ -2,18 +2,33 @@
 # Somewhat complete instruction set and operand type check.  No
 # relocations or deferred register definitions here.
 #
-Main:
-	# add/addu/sub/subu
-	add	$12, $23, $5
+_start:
+	# add/addu/sub/subu/mul/mulu/div/divu
+	add	$12, $23, $8
 	add	$12, $23, $8 << 2
-	add	$12, $23, $8 << 62
-	add	$22, $20, 255
-	add	$22, $20, 4000
+	add	$12, $23, 4000
+	addu	$12, $23, $8
 	addu	$12, $23, $8 << 2
-	addu	$22, $20, 4000
+	addu	$12, $23, 4000
+	sub	$12, $23, $8
 	sub	$12, $23, $8 << 2
-	sub	$22, $20, 4000
+	sub	$12, $23, 4000
+	subu	$12, $23, $8
 	subu	$12, $23, $8 << 2
-	subu	$22, $20, 4000
+	subu	$12, $23, 4000
+	mul	$12, $23, $8
+	mul	$12, $23, $8 << 2
+	mul	$12, $23, 4000
+	mulu	$12, $23, $8
+	mulu	$12, $23, $8 << 2
+	mulu	$12, $23, 4000
+	div	$12, $23, $8
+	div	$12, $23, $8 << 2
+	div	$12, $23, 4000
+	divu	$12, $23, $8
+	divu	$12, $23, $8 << 2
+	divu	$12, $23, 4000
 	# swym
 	swym	0, 0, 0
+	# nop
+	nop
