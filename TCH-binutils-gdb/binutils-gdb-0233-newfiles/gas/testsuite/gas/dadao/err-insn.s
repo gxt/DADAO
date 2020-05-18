3c385eb9ec37f160f93e8eaf93e1c2c55266ffa6
Guan Xuetao <gxt@pku.edu.cn>
2020-05-18 08:01:45 +0000

    TCH-bin: close err-insn.s testsuite
    Signed-off-by: Guan Xuetao <gxt@pku.edu.cn>

--- TCH-binutils-gdb/binutils-gdb-0233-newfiles/gas/testsuite/gas/dadao/err-insn.s
+++ TCH-binutils-gdb/binutils-gdb-0233-newfiles/gas/testsuite/gas/dadao/err-insn.s
@@ -1,4 +1,2 @@
 # { dg-do assemble { target dadao-*-* } }
-Main:
- FLOT $112,$223,$41 ;# { dg-error "invalid operands" "Y field of FLOT 1" }
- FLOT $112,$223,141 ;# { dg-error "invalid operands" "Y field of FLOT 2" }
+_start:
