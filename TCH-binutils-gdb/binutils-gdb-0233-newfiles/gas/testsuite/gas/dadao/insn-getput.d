92d3743a9f81fb2ffb041f310c242faba2db101b
Guan Xuetao <gxt@pku.edu.cn>
2020-06-11 10:19:36 +0000

    TCH-bin: adjust test case insn-getput.d
    Signed-off-by: Guan Xuetao <gxt@pku.edu.cn>

--- TCH-binutils-gdb/binutils-gdb-0233-newfiles/gas/testsuite/gas/dadao/insn-getput.d
+++ TCH-binutils-gdb/binutils-gdb-0233-newfiles/gas/testsuite/gas/dadao/insn-getput.d
@@ -5,11 +5,11 @@ tmpdir/insn-getput.o:     file format elf64-dadao
 Disassembly of section .text:
 
 0000000000000000 <_start>:
-   0:	da803040 	get.rp	rg3, sp
-   4:	da843080 	get.rf	rg3, rf2
-   8:	da8830c0 	get.rv	rg3, rv3
-   c:	da8c3100 	get.rs	rg3, rJ
-  10:	da401080 	put.rp	sp, rg2
-  14:	da442080 	put.rf	rf2, rg2
-  18:	da483080 	put.rv	rv3, rg2
-  1c:	da4c4080 	put.rs	rJ, rg2
+   0:	da843040 	get.rp	rg3, sp
+   4:	da883080 	get.rf	rg3, rf2
+   8:	da8c30c0 	get.rv	rg3, rv3
+   c:	da903100 	get.rs	rg3, rJ
+  10:	da441080 	put.rp	sp, rg2
+  14:	da482080 	put.rf	rf2, rg2
+  18:	da4c3080 	put.rv	rv3, rg2
+  1c:	da504080 	put.rs	rJ, rg2
