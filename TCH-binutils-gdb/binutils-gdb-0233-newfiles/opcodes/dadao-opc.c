e1ee0c8305b8be764a52bc6f98a943c2a020a33f
Guan Xuetao <gxt@pku.edu.cn>
2020-06-15 09:35:48 +0000

    TCH-bin: add 3 rg regs as datao regs
    Signed-off-by: Guan Xuetao <gxt@pku.edu.cn>

--- TCH-binutils-gdb/binutils-gdb-0233-newfiles/opcodes/dadao-opc.c
+++ TCH-binutils-gdb/binutils-gdb-0233-newfiles/opcodes/dadao-opc.c
@@ -15,7 +15,9 @@ const struct dadao_reg_alias dadao_reg_aliases[] =
  {
 	/* General registers */
 	{"zero", 0},
-	{"datao", 3},	/* internal reg for as, SHOULD removed at last */
+	{"datao1", 1},	/* internal reg for as, SHOULD removed at last */
+	{"datao2", 2},	/* internal reg for as, SHOULD removed at last */
+	{"datao3", 3},	/* internal reg for as, SHOULD removed at last */
 	/* Pointer registers */
 	{"pc", 0x40 + 0},
 	{"sp", 0x40 + 1},
