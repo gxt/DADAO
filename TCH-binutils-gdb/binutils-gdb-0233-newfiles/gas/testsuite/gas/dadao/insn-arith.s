e609f04fec224ef448a90a64868673f29b216b4e
Guan Xuetao <gxt@pku.edu.cn>
2020-07-03 22:32:18 +0800

    TCH-bin: adjust insn-arith test case
    Signed-off-by: Guan Xuetao <gxt@pku.edu.cn>

--- TCH-binutils-gdb/binutils-gdb-0233-newfiles/gas/testsuite/gas/dadao/insn-arith.s
+++ TCH-binutils-gdb/binutils-gdb-0233-newfiles/gas/testsuite/gas/dadao/insn-arith.s
@@ -3,25 +3,19 @@ _start:
 	# add/addu/sub/subu/mul/mulu/div/divu
 	add	rg12, rg23, rg8
 	add	rg12, rg23, rg8 << 2
-	add	rg12, rg23, 4000
+	add	rg12, rg23, 2000
 	addu	rg12, rg23, rg8
 	addu	rg12, rg23, rg8 << 2
-	addu	rg12, rg23, 4000
+	addu	rg12, rg23, 2000
 	sub	rg12, rg23, rg8
 	sub	rg12, rg23, rg8 << 2
-	sub	rg12, rg23, 4000
+	sub	rg12, rg23, 2000
 	subu	rg12, rg23, rg8
 	subu	rg12, rg23, rg8 << 2
-	subu	rg12, rg23, 4000
+	subu	rg12, rg23, 2000
 	mul	rg12, rg23, rg8
 	mul	rg12, rg23, rg8 << 2
-	mul	rg12, rg23, 4000
-	mulu	rg12, rg23, rg8
-	mulu	rg12, rg23, rg8 << 2
-	mulu	rg12, rg23, 4000
-	div	rg12, rg23, rg8
-	div	rg12, rg23, rg8 << 2
-	div	rg12, rg23, 4000
-	divu	rg12, rg23, rg8
-	divu	rg12, rg23, rg8 << 2
-	divu	rg12, rg23, 4000
+	mul	rg12, rg23, 2000
+	mulu	rg12, rg23, rg8, rg4
+	div	rg12, rg23, rg8, rg4
+	divu	rg12, rg23, rg8, rg4
