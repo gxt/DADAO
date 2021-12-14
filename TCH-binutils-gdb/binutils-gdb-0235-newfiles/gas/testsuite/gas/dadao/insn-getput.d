e533680760c4d6dd5b45a11e6a4955096b405d8e
Yang Chen <yang_chen@pku.edu.cn>
2021-12-14 13:46:48 +0800

    TCH-bin: insn-getput
    Signed-off-by: Yang Chen <yang_chen@pku.edu.cn>

--- TCH-binutils-gdb/binutils-gdb-0235-newfiles/gas/testsuite/gas/dadao/insn-getput.d
+++ TCH-binutils-gdb/binutils-gdb-0235-newfiles/gas/testsuite/gas/dadao/insn-getput.d
@@ -5,11 +5,10 @@ tmpdir/insn-getput.o:     file format elf64-dadao
 Disassembly of section .text:
 
 0000000000000000 <_start>:
-   0:	da843040 	get.rp	rg3, sp
-   4:	da883080 	get.rf	rg3, rf2
-   8:	da8c30c0 	get.rv	rg3, rv3
-   c:	da903100 	get.rs	rg3, rJ
-  10:	da441080 	put.rp	sp, rg2
-  14:	da482080 	put.rf	rf2, rg2
-  18:	da4c3080 	put.rv	rv3, rg2
-  1c:	da504080 	put.rs	rJ, rg2
+   0:	10c01040 	rg2rg	rg1, rg1, 0
+   4:	10c41041 	rg2rp	rp1, rg1, 1
+   8:	10c81042 	rg2rf	rf1, rg1, 2
+   c:	10d01043 	rp2rg	rg1, rp1, 3
+  10:	10d41044 	rp2rp	rp1, rp1, 4
+  14:	10e01045 	rf2rg	rg1, rf1, 5
+  18:	10e81046 	rf2rf	rf1, rf1, 6
--- TCH-binutils-gdb/binutils-gdb-0235-newfiles/gas/testsuite/gas/dadao/insn-getput.s
+++ TCH-binutils-gdb/binutils-gdb-0235-newfiles/gas/testsuite/gas/dadao/insn-getput.s
@@ -2,11 +2,10 @@
 	.text
 	.global _start
 _start:
-	get.rp	rg3, rp1
-	get.rf	rg3, rf2
-	get.rv	rg3, rv3
-	get.rs	rg3, rJ
-	put.rp	rp1, rg2
-	put.rf	rf2, rg2
-	put.rv	rv3, rg2
-	put.rs	rJ, rg2
+	rg2rg	rg1, rg1, 0
+	rg2rp	rp1, rg1, 1
+	rg2rf	rf1, rg1, 2
+	rp2rg	rg1, rp1, 3
+	rp2rp	rp1, rp1, 4
+	rf2rg	rg1, rf1, 5
+	rf2rf	rf1, rf1, 6
