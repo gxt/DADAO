6d3375285c792147f597324e9cab4a48ff8ce989
Yang Chen <yang_chen@pku.edu.cn>
2021-12-14 16:37:03 +0800

    TCH-bin: adjust insn-trap
    Signed-off-by: Yang Chen <yang_chen@pku.edu.cn>

--- TCH-binutils-gdb/binutils-gdb-0235-newfiles/gas/testsuite/gas/dadao/insn-trap.s
+++ TCH-binutils-gdb/binutils-gdb-0235-newfiles/gas/testsuite/gas/dadao/insn-trap.s
@@ -2,8 +2,8 @@
 	.text
 	.global _start
 _start:
-	trap	cp1, 262144
-	cmd 	cp1, 262144
+	trap	cp1, 262143
+	cmd 	cp1, 262143
 	rg2cr	cp1, cr1, cr2, rg1
 	cr2rg	cp1, rg1, cr1, cr2
     cpld    cp1, rp1, 4096
