04f62ca8f10ba7b00fb421788727b0e8814c8c6a
Yang Chen <yang_chen@pku.edu.cn>
2021-12-14 13:52:15 +0800

    TCH-bin: insn-jsr
    Signed-off-by: Yang Chen <yang_chen@pku.edu.cn>

--- TCH-binutils-gdb/binutils-gdb-0235-newfiles/gas/testsuite/gas/dadao/insn-jsr.d
+++ TCH-binutils-gdb/binutils-gdb-0235-newfiles/gas/testsuite/gas/dadao/insn-jsr.d
@@ -5,21 +5,21 @@ tmpdir/insn-jsr.o:     file format elf64-dadao
 Disassembly of section .text:
 
 0000000000000000 <_start>:
-   0:	d1000001 	call	8 <_rec1>
-   4:	dadc0000 	ret
+   0:	6c000001 	call	8 <_rec1>
+   4:	6e000000 	ret
 
 0000000000000008 <_rec1>:
-   8:	d1000002 	call	14 <_rec2>
-   c:	d1000001 	call	14 <_rec2>
-  10:	dadc0000 	ret
+   8:	6c000002 	call	14 <_rec2>
+   c:	6c000001 	call	14 <_rec2>
+  10:	6e000000 	ret
 
 0000000000000014 <_rec2>:
-  14:	d1000004 	call	28 <_rec3>
-  18:	d1000003 	call	28 <_rec3>
-  1c:	d1000002 	call	28 <_rec3>
-  20:	d1000001 	call	28 <_rec3>
-  24:	dadc0000 	ret
+  14:	6c000004 	call	28 <_rec3>
+  18:	6c000003 	call	28 <_rec3>
+  1c:	6c000002 	call	28 <_rec3>
+  20:	6c000001 	call	28 <_rec3>
+  24:	6e000000 	ret
 
 0000000000000028 <_rec3>:
-  28:	d1fffff7 	call	8 <_rec1>
-  2c:	dadc0000 	ret
+  28:	6cfffff7 	call	8 <_rec1>
+  2c:	6e000000 	ret
