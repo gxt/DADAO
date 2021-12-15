e839d1b8bed126f143b4f513475168c4cbd6ec5b
Guan Xuetao <gxt@pku.edu.cn>
2021-12-15 08:46:48 +0800

    TCH-bin: change cmd/cpld/cpst to cpcoop/cpload/cpstor
    Signed-off-by: Guan Xuetao <gxt@pku.edu.cn>

--- TCH-binutils-gdb/binutils-gdb-0235-newfiles/gas/testsuite/gas/dadao/insn-trap.d
+++ TCH-binutils-gdb/binutils-gdb-0235-newfiles/gas/testsuite/gas/dadao/insn-trap.d
@@ -6,8 +6,8 @@ Disassembly of section .text:
 
 0000000000000000 <_start>:
    0:	7007ffff 	trap	cp1, 262143
-   4:	7107ffff 	cmd 	cp1, 262143
+   4:	7107ffff 	cpcoop	cp1, 262143
    8:	72041041 	rg2cr	cp1, cr1, cr2, rg1
    c:	73041041 	cr2rg	cp1, rg1, cr1, cr2
-  10:	74041fff 	cpld    cp1, rp1, 4095
-  14:	75041fff 	cpst    cp1, rp1, 4095
+  10:	74041fff 	cpload	cp1, rp1, 4095
+  14:	75041fff 	cpstor	cp1, rp1, 4095
--- TCH-binutils-gdb/binutils-gdb-0235-newfiles/gas/testsuite/gas/dadao/insn-trap.s
+++ TCH-binutils-gdb/binutils-gdb-0235-newfiles/gas/testsuite/gas/dadao/insn-trap.s
@@ -3,8 +3,8 @@
 	.global _start
 _start:
 	trap	cp1, 262143
-	cmd 	cp1, 262143
+	cpcoop	cp1, 262143
 	rg2cr	cp1, cr1, cr2, rg1
 	cr2rg	cp1, rg1, cr1, cr2
-    cpld    cp1, rp1, 4096
-    cpst    cp1, rp1, 4096
+	cpload	cp1, rp1, 4096
+	cpstor	cp1, rp1, 4096
