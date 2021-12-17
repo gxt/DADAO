7ed0987f651dda90806b85607a3d67c7cdef5c17
Yang Chen <yang_chen@pku.edu.cn>
2021-12-17 14:47:06 +0800

    TCH-bin: change insn-seto
    Signed-off-by: Yang Chen <yang_chen@pku.edu.cn>

--- TCH-binutils-gdb/binutils-gdb-0235-newfiles/gas/testsuite/gas/dadao/insn-seto.d
+++ TCH-binutils-gdb/binutils-gdb-0235-newfiles/gas/testsuite/gas/dadao/insn-seto.d
@@ -5,13 +5,20 @@ tmpdir/insn-seto.o:     file format elf64-dadao
 Disassembly of section .text:
 
 0000000000000000 <_start>:
-   0:	3a05abcd 	setwk	rg1, 0xabcd
-   4:	3b04abcd 	incwl	rg1, 0xabcd
-   8:	3a0a0013 	setwj	rg2, 0x13
-   c:	3b095792 	incwk	rg2, 0x5792
-  10:	3b084680 	incwl	rg2, 0x4680
-  14:	3a131234 	setwh	rg4, 0x1234
-  18:	3b125678 	incwj	rg4, 0x5678
-  1c:	3b111234 	incwk	rg4, 0x1234
-  20:	3b105678 	incwl	rg4, 0x5678
-  24:	3a140000 	setwl	rg5, 0x0
+   0:	1504abcd 	setzwl	rg1, 0xabcd
+   4:	1605abcd 	orwk	rg1, 0xabcd
+   8:	15084680 	setzwl	rg2, 0x4680
+   c:	16095792 	orwk	rg2, 0x5792
+  10:	160a0013 	orwj	rg2, 0x13
+  14:	15105678 	setzwl	rg4, 0x5678
+  18:	16111234 	orwk	rg4, 0x1234
+  1c:	16125678 	orwj	rg4, 0x5678
+  20:	16131234 	orwh	rg4, 0x1234
+  24:	15140000 	setzwl	rg5, 0x0
+  28:	1418ffff 	setowl	rg6, 0xffff
+  2c:	151cfffe 	setzwl	rg7, 0xfffe
+  30:	161dffff 	orwk	rg7, 0xffff
+  34:	1520ffff 	setzwl	rg8, 0xffff
+  38:	1622ffff 	orwj	rg8, 0xffff
+  3c:	14240000 	setowl	rg9, 0x0
+  40:	17260000 	andnwj	rg9, 0x0
--- TCH-binutils-gdb/binutils-gdb-0235-newfiles/gas/testsuite/gas/dadao/insn-seto.s
+++ TCH-binutils-gdb/binutils-gdb-0235-newfiles/gas/testsuite/gas/dadao/insn-seto.s
@@ -2,7 +2,11 @@
 	.text
 	.global _start
 _start:
-	seto	rg1, 0xabcdabcd
-	seto	rg2, 0x1357924680
-	seto	rg4, 0x1234567812345678
-	seto	rg5, 0
+	setrg	rg1, 0xabcdabcd
+	setrg	rg2, 0x1357924680
+	setrg	rg4, 0x1234567812345678
+	setrg	rg5, 0
+	setrg	rg6, -1
+	setrg	rg7, 0xFFFFFFFE
+	setrg	rg8, 0xFFFF0000FFFF
+	setrg	rg9, 0xFFFF0000FFFF0000
