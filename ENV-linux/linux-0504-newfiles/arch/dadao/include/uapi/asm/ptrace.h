2135d04988eaac1565b674400a61c9d3144e9a90
Guan Xuetao <gxt@pku.edu.cn>
2021-12-26 14:03:44 +0800

    ENV-linux: add simple ptrace.h and sigcontext.h
    Signed-off-by: Guan Xuetao <gxt@pku.edu.cn>

--- ENV-linux/linux-0504-newfiles/arch/dadao/include/uapi/asm/ptrace.h
+++ ENV-linux/linux-0504-newfiles/arch/dadao/include/uapi/asm/ptrace.h
@@ -4,4 +4,11 @@
 #ifndef __DADAO_UAPI_PTRACE_H__
 #define __DADAO_UAPI_PTRACE_H__
 
+struct pt_regs {
+	unsigned long long rd_regs[64];
+	unsigned long long rb_regs[64];
+	unsigned long long rf_regs[64];
+	unsigned long long ra_regs[64];
+};
+
 #endif /* __DADAO_UAPI_PTRACE_H__ */
--- ENV-linux/linux-0504-newfiles/arch/dadao/include/uapi/asm/sigcontext.h
+++ ENV-linux/linux-0504-newfiles/arch/dadao/include/uapi/asm/sigcontext.h
@@ -4,4 +4,10 @@
 #ifndef __DADAO_UAPI_SIGCONTEXT_H__
 #define __DADAO_UAPI_SIGCONTEXT_H__
 
+#include <asm/ptrace.h>
+
+struct sigcontext {
+	struct pt_regs sc_regs;
+};
+
 #endif /* __DADAO_UAPI_SIGCONTEXT_H__ */
