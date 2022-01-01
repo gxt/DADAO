45216e938e7e57322affa957feafd5ac65f4320a
Guan Xuetao <gxt@pku.edu.cn>
2022-01-01 15:38:55 +0800

    ENV-linux: add arch/dadao/include/uapi/asm/bitsperlong.h
    Signed-off-by: Guan Xuetao <gxt@pku.edu.cn>

---
+++ ENV-linux/linux-0504-newfiles/arch/dadao/include/uapi/asm/bitsperlong.h
@@ -0,0 +1,12 @@
+/*
+ * Copyright (C) 2020-2033 Guan Xuetao (AT) Peking Univ.
+ * Contributed by Guan Xuetao <gxt@pku.edu.cn>
+ */
+#ifndef __DADAO_UAPI_BITSPERLONG_H__
+#define __DADAO_UAPI_BITSPERLONG_H__
+
+#define __BITS_PER_LONG		64
+
+#include <asm-generic/bitsperlong.h>
+
+#endif /* __DADAO_UAPI_BITSPERLONG_H__ */
