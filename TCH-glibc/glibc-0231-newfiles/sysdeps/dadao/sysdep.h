5a417159e8bcc07dfe86830aeb931a7f0466f671
Guan Xuetao <gxt@pku.edu.cn>
2022-01-03 19:39:37 +0800

    TCH-glibc: add ENTRY and END in sysdeps/dadao/sysdep.h
    Signed-off-by: Guan Xuetao <gxt@pku.edu.cn>

--- TCH-glibc/glibc-0231-newfiles/sysdeps/dadao/sysdep.h
+++ TCH-glibc/glibc-0231-newfiles/sysdeps/dadao/sysdep.h
@@ -7,4 +7,32 @@
 
 #include <sysdeps/generic/sysdep.h>
 
+#ifdef	__ASSEMBLER__
+
+/* Syntactic details of assembler.  */
+#define	ASM_SIZE_DIRECTIVE(name)	.size name,.-name
+
+#ifdef	PROF
+/* FIXME: add mcount support lator */
+# define	CALL_MCOUNT		swym		/* Do nothing.  */
+#else	/* PROF */
+# define	CALL_MCOUNT				/* Do nothing.  */
+#endif	/* PROF */
+
+/* Define an entry point visible from C.  */
+#define	ENTRY(name)						\
+	.global	C_SYMBOL_NAME(name);				\
+	.type	C_SYMBOL_NAME(name),%function;			\
+	.align	4;						\
+	C_LABEL(name)						\
+	cfi_startproc;						\
+	CALL_MCOUNT
+
+#undef	END
+#define	END(name)						\
+	cfi_endproc;						\
+	ASM_SIZE_DIRECTIVE(name)
+
+#endif	/* __ASSEMBLER__ */
+
 #endif /* _SYSDEPS_DADAO_SYSDEP_H_ */
