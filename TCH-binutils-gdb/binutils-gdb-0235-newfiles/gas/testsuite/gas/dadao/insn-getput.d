ec67edc7d594d807c8391c5293b132841e63a5ac
Yang Chen <yang_chen@pku.edu.cn>
2022-01-04 17:21:31 +0800

    TCH-bin: insn-getput testsuite
    Signed-off-by: Yang Chen <yang_chen@pku.edu.cn>

--- TCH-binutils-gdb/binutils-gdb-0235-newfiles/gas/testsuite/gas/dadao/insn-getput.d
+++ TCH-binutils-gdb/binutils-gdb-0235-newfiles/gas/testsuite/gas/dadao/insn-getput.d
@@ -5,17 +5,15 @@ tmpdir/insn-getput.o:     file format elf64-dadao
 Disassembly of section .text:
 
 0000000000000000 <_start>:
-   0:	10a01040 	rd2rd	rd1, rd1, 0
-   4:	10a41041 	rd2rb	rb1, rd1, 1
-   8:	10a81042 	rb2rd	rd1, rb1, 2
-   c:	10ae1043 	rb2rb	rb1, rb1, 3
-  10:	10b41042    cmprb rd1, rb1, rb2
-  14:	10b81081    addrb rb1, rb2, rd1
-  18: 10bc1081    subrb rb1, rb2, rd1
-  1c: 10c41040 	rd2rf	rf1, rd1, 0
-  20:	10c81041 	rf2rd	rd1, rf1, 1
-  24: 10ce1042 	rf2rf	rf1, rf1, 2
-  28: 10e41043    rd2ra ra1, rd1, 3
-  2c: 10e81044    ra2rd rd1, ra1, 4
-
-  
+   0:	10a0107f 	rd2rd	rd1, rd1, 63
+   4:	10a4107f 	rd2rb	rb1, rd1, 63
+   8:	10a8107f 	rb2rd	rd1, rb1, 63
+   c:	10ac107f 	rb2rb	rb1, rb1, 63
+  10:	10b41042 	cmprb	rd1, rb1, rb2
+  14:	10b81081 	addrb	rb1, rb2, rd1
+  18:	10bc1081 	subrb	rb1, rb2, rd1
+  1c:	10c4107f 	rd2rf	rf1, rd1, 63
+  20:	10c8107f 	rf2rd	rd1, rf1, 63
+  24:	10cc107f 	rf2rf	rf1, rf1, 63
+  28:	10e4107f 	rd2ra	ra1, rd1, 63
+  2c:	10e8107f 	ra2rd	rd1, ra1, 63
--- TCH-binutils-gdb/binutils-gdb-0235-newfiles/gas/testsuite/gas/dadao/insn-getput.s
+++ TCH-binutils-gdb/binutils-gdb-0235-newfiles/gas/testsuite/gas/dadao/insn-getput.s
@@ -2,17 +2,17 @@
 	.text
 	.global _start
 _start:
-	rd2rd	rd1, rd1, 0
-	rd2rb	rb1, rd1, 1
-	rb2rd	rd1, rb1, 2
-	rb2rb	rb1, rb1, 3
+	rd2rd	rd1, rd1, 63
+	rd2rb	rb1, rd1, 63
+	rb2rd	rd1, rb1, 63
+	rb2rb	rb1, rb1, 63
 	cmprb	rd1, rb1, rb2
 	addrb	rb1, rb2, rd1
 	subrb	rb1, rb2, rd1
 
-	rd2rf	rf1, rd1, 0
-	rf2rd	rd1, rf1, 1
-	rf2rf	rf1, rf1, 2
+	rd2rf	rf1, rd1, 63
+	rf2rd	rd1, rf1, 63
+	rf2rf	rf1, rf1, 63
 
-	rd2ra	ra1, rd1, 3
-	ra2rd	rd1, ra1, 4
+	rd2ra	ra1, rd1, 63
+	ra2rd	rd1, ra1, 63
