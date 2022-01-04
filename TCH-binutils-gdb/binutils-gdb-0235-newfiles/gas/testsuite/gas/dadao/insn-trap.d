cf5179a1e8e99242062478bb40e2b1b8942536ed
Yang Chen <yang_chen@pku.edu.cn>
2022-01-04 17:20:34 +0800

    TCH-bin: insn-trap testsuite
    Signed-off-by: Yang Chen <yang_chen@pku.edu.cn>

--- TCH-binutils-gdb/binutils-gdb-0235-newfiles/gas/testsuite/gas/dadao/insn-trap.d
+++ TCH-binutils-gdb/binutils-gdb-0235-newfiles/gas/testsuite/gas/dadao/insn-trap.d
@@ -7,7 +7,7 @@ Disassembly of section .text:
 0000000000000000 <_start>:
    0:	7007ffff 	trap	cp1, 262143
    4:	7107ffff 	cpcoop	cp1, 262143
-   8:	72041041 	rd2cr	cp1, cr1, cr2, rd1
-   c:	73041041 	cr2rd	cp1, rd1, cr1, cr2
+   8:	72041081 	rd2cr	cp1, cr1, cr2, rd1
+   c:	73041042 	cr2rd	cp1, rd1, cr1, cr2
   10:	74041fff 	cpload	cp1, rb1, 4095
   14:	75041fff 	cpstor	cp1, rb1, 4095
--- TCH-binutils-gdb/binutils-gdb-0235-newfiles/gas/testsuite/gas/dadao/insn-trap.s
+++ TCH-binutils-gdb/binutils-gdb-0235-newfiles/gas/testsuite/gas/dadao/insn-trap.s
@@ -6,5 +6,5 @@ _start:
 	cpcoop	cp1, 262143
 	rd2cr	cp1, cr1, cr2, rd1
 	cr2rd	cp1, rd1, cr1, cr2
-	cpload	cp1, rb1, 4096
-	cpstor	cp1, rb1, 4096
+	cpload	cp1, rb1, 4095
+	cpstor	cp1, rb1, 4095
