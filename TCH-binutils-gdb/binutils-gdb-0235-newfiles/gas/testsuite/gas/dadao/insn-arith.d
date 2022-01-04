ac45322206caf9c4df31f0f7e2bd6e7449a3d179
Yang Chen <yang_chen@pku.edu.cn>
2022-01-04 17:21:58 +0800

    TCH-bin: insn-arith testsuite
    Signed-off-by: Yang Chen <yang_chen@pku.edu.cn>

--- TCH-binutils-gdb/binutils-gdb-0235-newfiles/gas/testsuite/gas/dadao/insn-arith.d
+++ TCH-binutils-gdb/binutils-gdb-0235-newfiles/gas/testsuite/gas/dadao/insn-arith.d
@@ -1,3 +1,4 @@
+insn-arith.d
 # objdump: -dr
 tmpdir/insn-arith.o:     file format elf64-dadao
 
@@ -5,19 +6,21 @@ tmpdir/insn-arith.o:     file format elf64-dadao
 Disassembly of section .text:
 
 0000000000000000 <_start>:
-   0:	1905ffff 	add	rd1, 131071
-   4:	19060000 	add	rd1, -131072
-   8:	1a0420c4 	add	rd1, rd2, rd3, rd4
-   c:	1b0420c4 	sub	rd1, rd2, rd3, rd4
-  10:	1c0420c4 	muls	rd1, rd2, rd3, rd4
-  14:	1d0420c4 	mulu	rd1, rd2, rd3, rd4
-  18:	1e0420c4 	divs	rd1, rd2, rd3, rd4
-  1c:	1f0420c4 	divu	rd1, rd2, rd3, rd4
-  20:	10201083 	and	rd1, rd2, rd3
-  24:	10241083 	orr	rd1, rd2, rd3
-  28:	10281083 	xor	rd1, rd2, rd3
-  2c:	120427ff 	cmps	rd1, rd2, 2047
-  30:	12042800 	cmps  rd1, rd2, -2048
-  34:	13042fff 	cmpu  rd1, rd2, 4095
-  38:	10901083 	cmps  rd1, rd2, rd3
-  3c:	10941083 	cmpu	rd1, rd2, rd3
+   0:	1805ffff 	addrb	rb1, 131071
+   4:	18060000 	addrb	rb1, -131072
+   8:	1905ffff 	add	rd1, 131071
+   c:	19060000 	add	rd1, -131072
+  10:	1a0420c4 	add	rd1, rd2, rd3, rd4
+  14:	1b0420c4 	sub	rd1, rd2, rd3, rd4
+  18:	1c0420c4 	muls	rd1, rd2, rd3, rd4
+  1c:	1d0420c4 	mulu	rd1, rd2, rd3, rd4
+  20:	1e0420c4 	divs	rd1, rd2, rd3, rd4
+  24:	1f0420c4 	divu	rd1, rd2, rd3, rd4
+  28:	10201083 	and	rd1, rd2, rd3
+  2c:	10241083 	orr	rd1, rd2, rd3
+  30:	10281083 	xor	rd1, rd2, rd3
+  34:	120427ff 	cmps	rd1, rd2, 2047
+  38:	12042800 	cmps	rd1, rd2, -2048
+  3c:	13042fff 	cmpu	rd1, rd2, 4095
+  40:	10901083 	cmps	rd1, rd2, rd3
+  44:	10941083 	cmpu	rd1, rd2, rd3
--- TCH-binutils-gdb/binutils-gdb-0235-newfiles/gas/testsuite/gas/dadao/insn-arith.s
+++ TCH-binutils-gdb/binutils-gdb-0235-newfiles/gas/testsuite/gas/dadao/insn-arith.s
@@ -1,19 +1,24 @@
 # Da Dao Zhi Jian
 _start:
-	# add/addu/sub/subu/mul
+	# addrb/add/sub
+	addrb	rb1, 131071
+	addrb	rb1, -131072
 	add	rd1, 131071
 	add	rd1, -131072
 	add	rd1, rd2, rd3, rd4
 	sub	rd1, rd2, rd3, rd4
+
 	# muls/mulu/divs/divu
 	muls	rd1, rd2, rd3, rd4
 	mulu	rd1, rd2, rd3, rd4
 	divs	rd1, rd2, rd3, rd4
 	divu	rd1, rd2, rd3, rd4
+
 	# and/orr/xor
 	and	rd1, rd2, rd3
 	orr	rd1, rd2, rd3
 	xor	rd1, rd2, rd3
+	
 	# cmps/cmpu
 	cmps	rd1, rd2, 2047
 	cmps	rd1, rd2, -2048
