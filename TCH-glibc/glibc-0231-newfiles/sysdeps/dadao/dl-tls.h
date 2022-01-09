93032d786eec97ff94318df861a29ff72f56723b
Guan Xuetao <gxt@pku.edu.cn>
2022-01-09 18:43:15 +0800

    TCH-glibc: add sysdeps/dadao/dl-tls.h
    Signed-off-by: Guan Xuetao <gxt@pku.edu.cn>

---
+++ TCH-glibc/glibc-0231-newfiles/sysdeps/dadao/dl-tls.h
@@ -0,0 +1,13 @@
+/*
+ * Copyright (C) 2020-2033 Guan Xuetao (AT) Peking Univ.
+ * Contributed by Guan Xuetao <gxt@pku.edu.cn>
+ */
+
+/* Type used for the representation of TLS information in the GOT.  */
+typedef struct
+{
+  unsigned long int ti_module;
+  unsigned long int ti_offset;
+} tls_index;
+
+extern void *__tls_get_addr (tls_index *ti);
