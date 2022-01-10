90824bd82e8c83f5f9e0887f445ccd4ae9e63a14
Guan Xuetao <gxt@pku.edu.cn>
2022-01-10 12:05:55 +0800

    ENV-linux: add arch/dadao/include/uapi/asm/unistd.h
    Signed-off-by: Guan Xuetao <gxt@pku.edu.cn>

---
+++ ENV-linux/linux-0504-newfiles/arch/dadao/include/uapi/asm/unistd.h
@@ -0,0 +1,12 @@
+/*
+ * Copyright (C) 2020-2033 Guan Xuetao (AT) Peking Univ.
+ * Contributed by Guan Xuetao <gxt@pku.edu.cn>
+ */
+#ifndef __DADAO_UAPI_UNISTD_H__
+#define __DADAO_UAPI_UNISTD_H__
+
+#define __ARCH_WANT_NEW_STAT
+
+#include <asm-generic/unistd.h>
+
+#endif /* __DADAO_UAPI_UNISTD_H__ */
