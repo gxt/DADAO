65e98802bddfdb99ec530f9e5cd5c35274d14c1d
Guan Xuetao <gxt@pku.edu.cn>
2022-01-12 22:00:38 +0800

    TCH-bin: replace next-pc with current-pc during relocation
    Signed-off-by: Guan Xuetao <gxt@pku.edu.cn>

--- TCH-binutils-gdb/binutils-gdb-0235-newfiles/bfd/elf64-dadao.c
+++ TCH-binutils-gdb/binutils-gdb-0235-newfiles/bfd/elf64-dadao.c
@@ -795,7 +795,7 @@ dadao_final_link_relocate (reloc_howto_type *howto, asection *input_section,
 		addr_abs = relocation + (bfd_vma)r_addend;
 		addr_rel = addr_abs - (input_section->output_section->vma
 			+ input_section->output_offset
-			+ r_offset + 4);
+			+ r_offset);
 
 		r = dadao_elf_perform_relocation (input_section, howto, contents,
 					addr_abs, addr_rel, error_message);
