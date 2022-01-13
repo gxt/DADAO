ee0f423d7c7fe408727ff96c5854e8c6a556fbfd
Guan Xuetao <gxt@pku.edu.cn>
2022-01-13 21:55:17 +0800

    ENV-linux: add arch/dadao/include/asm/barrier.h
    Signed-off-by: Guan Xuetao <gxt@pku.edu.cn>

---
+++ ENV-linux/linux-0504-newfiles/arch/dadao/include/asm/barrier.h
@@ -0,0 +1,16 @@
+/*
+ * Copyright (C) 2020-2033 Guan Xuetao (AT) Peking Univ.
+ * Contributed by Guan Xuetao <gxt@pku.edu.cn>
+ */
+#ifndef __DADAO_ASM_BARRIER_H__
+#define __DADAO_ASM_BARRIER_H__
+
+#ifndef __ASSEMBLY__
+
+#define nop()		asm volatile ("swym")
+
+#include <asm-generic/barrier.h>
+
+#endif	/* __ASSEMBLY__ */
+
+#endif /* __DADAO_ASM_BARRIER_H__ */
