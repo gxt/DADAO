94770b5aad859274e60d162a4a1284fbe71944d3
Guan Xuetao <gxt@pku.edu.cn>
2022-01-16 21:16:53 +0800

    ENV-linux: add empty arch/dadao/kernel/asm-offsets.c
    Signed-off-by: Guan Xuetao <gxt@pku.edu.cn>

---
+++ ENV-linux/linux-0504-newfiles/arch/dadao/kernel/asm-offsets.c
@@ -0,0 +1,15 @@
+/*
+ * Copyright (C) 2020-2033 Guan Xuetao (AT) Peking Univ.
+ * Contributed by Guan Xuetao <gxt@pku.edu.cn>
+ */
+#include <linux/sched.h>
+#include <linux/mm.h>
+#include <linux/thread_info.h>
+
+int main(void)
+{
+	BLANK();
+	BLANK();
+	BLANK();
+	return 0;
+}
