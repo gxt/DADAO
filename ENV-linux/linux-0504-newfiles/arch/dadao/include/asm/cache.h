64216b06ee8df164d14ad19f468df885d191d6e8
Guan Xuetao <gxt@pku.edu.cn>
2022-01-16 19:28:41 +0800

    ENV-linux: add cache.h and set L1_CACHE_SHIFT to be 6
    Signed-off-by: Guan Xuetao <gxt@pku.edu.cn>

---
+++ ENV-linux/linux-0504-newfiles/arch/dadao/include/asm/cache.h
@@ -0,0 +1,11 @@
+/*
+ * Copyright (C) 2020-2033 Guan Xuetao (AT) Peking Univ.
+ * Contributed by Guan Xuetao <gxt@pku.edu.cn>
+ */
+#ifndef __DADAO_ASM_CACHE_H__
+#define __DADAO_ASM_CACHE_H__
+
+#define L1_CACHE_SHIFT		6
+#define L1_CACHE_BYTES		(1 << L1_CACHE_SHIFT)
+
+#endif /* __DADAO_ASM_CACHE_H__ */
