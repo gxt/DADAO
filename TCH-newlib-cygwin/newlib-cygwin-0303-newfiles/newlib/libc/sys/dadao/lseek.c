269206b17563c0494c470d25bce86810cf12fde0
Hao Chenqi <hchenqi@pku.edu.cn>
2022-03-31 16:07:11 +0800

    TCH-newlib: implement lseek
    Signed-off-by: Hao Chenqi <hchenqi@pku.edu.cn>

--- TCH-newlib-cygwin/newlib-cygwin-0303-newfiles/newlib/libc/sys/dadao/lseek.c
+++ TCH-newlib-cygwin/newlib-cygwin-0303-newfiles/newlib/libc/sys/dadao/lseek.c
@@ -1,6 +1,18 @@
 
 int lseek(int file, int ptr, int dir) {
-  return 0;
+  int ret;
+  __asm__ (
+    "setzwl  rd15, 62     \n"
+    "rd2rd   rd16, %1, 0  \n"
+    "rd2rd   rd17, %2, 0  \n"
+    "rd2rd   rd18, %3, 0  \n"
+    "trap    cp0, 0       \n"
+    "rd2rd   %0, rd31, 0  \n"
+    : "=r" (ret)
+    : "r" (file), "r" (ptr), "r" (dir)
+    : "memory"
+  );
+  return ret;
 }
 
 int _lseek(int file, int ptr, int dir) {
