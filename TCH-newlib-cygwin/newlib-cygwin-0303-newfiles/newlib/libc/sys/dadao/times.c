254bd32e66e2cdd50ba3e477cefc4d346e1a3695
Hao Chenqi <hchenqi@pku.edu.cn>
2022-03-31 16:07:11 +0800

    TCH-newlib: implement times
    Signed-off-by: Hao Chenqi <hchenqi@pku.edu.cn>

--- TCH-newlib-cygwin/newlib-cygwin-0303-newfiles/newlib/libc/sys/dadao/times.c
+++ TCH-newlib-cygwin/newlib-cygwin-0303-newfiles/newlib/libc/sys/dadao/times.c
@@ -1,12 +1,16 @@
-#include <_ansi.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include "sys/syscall.h"
 #include "sys/times.h"
 
-clock_t
-_times (struct tms *buffer)
+clock_t _times(struct tms *buffer)
 {
-  memset (buffer, 0, sizeof (*buffer));
-  return 0;
+  clock_t ret;
+  __asm__ (
+    "setzwl  rd15, 153    \n"
+    "rd2rd   rd16, %1, 0  \n"
+    "trap    cp0, 0       \n"
+    "rd2rd   %0, rd31, 0  \n"
+    : "=r" (ret)
+    : "r" (buffer)
+    : "memory"
+  );
+  return ret;
 }
