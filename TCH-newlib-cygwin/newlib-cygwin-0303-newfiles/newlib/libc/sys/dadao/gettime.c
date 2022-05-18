1ec6a10dd195d98aa971d78fbd4337d92b659585
Hao Chenqi <hchenqi@pku.edu.cn>
2022-05-18 21:05:43 +0800

    TCH-newlib: impl gettimeofday and clock_gettime
    Signed-off-by: Hao Chenqi <hchenqi@pku.edu.cn>

--- TCH-newlib-cygwin/newlib-cygwin-0303-newfiles/newlib/libc/sys/dadao/gettime.c
+++ TCH-newlib-cygwin/newlib-cygwin-0303-newfiles/newlib/libc/sys/dadao/gettime.c
@@ -1,14 +1,64 @@
-#include <_ansi.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include "sys/syscall.h"
 #include "sys/time.h"
-#include <errno.h>
+#include "time.h"
 
-int
-_gettimeofday (struct timeval *tp, void *tzp)
-{
-  UNIMPLEMENTED (("timeval: %p, tzp: %p", tp, tzp));
-  errno = ENOSYS;
-  return -1;
+
+int _gettimeofday(struct timeval* tv, struct timezone* tz) {
+  int ret;
+  __asm__ (
+    "setzwl  rd15, 169    \n"
+    "rd2rd   rd16, %1, 0  \n"
+    "rd2rd   rd17, %2, 0  \n"
+    "trap    cp0, 0       \n"
+    "rd2rd   %0, rd31, 0  \n"
+    : "=r" (ret)
+    : "r" (tv), "r" (tz)
+    : "memory"
+  );
+  return ret;
+}
+
+int _settimeofday(const struct timeval *tv, const struct timezone *tz) {
+  int ret;
+  __asm__ (
+    "setzwl  rd15, 170    \n"
+    "rd2rd   rd16, %1, 0  \n"
+    "rd2rd   rd17, %2, 0  \n"
+    "trap    cp0, 0       \n"
+    "rd2rd   %0, rd31, 0  \n"
+    : "=r" (ret)
+    : "r" (tv), "r" (tz)
+    : "memory"
+  );
+  return ret;
+}
+
+
+int clock_gettime(clockid_t clk_id, struct timespec *tp) {
+  int ret;
+  __asm__ (
+    "setzwl  rd15, 113    \n"
+    "rd2rd   rd16, %1, 0  \n"
+    "rd2rd   rd17, %2, 0  \n"
+    "trap    cp0, 0       \n"
+    "rd2rd   %0, rd31, 0  \n"
+    : "=r" (ret)
+    : "r" (clk_id), "r" (tp)
+    : "memory"
+  );
+  return ret;
+}
+
+int clock_settime(clockid_t clk_id, const struct timespec *tp) {
+  int ret;
+  __asm__ (
+    "setzwl  rd15, 112    \n"
+    "rd2rd   rd16, %1, 0  \n"
+    "rd2rd   rd17, %2, 0  \n"
+    "trap    cp0, 0       \n"
+    "rd2rd   %0, rd31, 0  \n"
+    : "=r" (ret)
+    : "r" (clk_id), "r" (tp)
+    : "memory"
+  );
+  return ret;
 }
