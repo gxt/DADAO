fd42355aa72306f66f7187728e2ed06c5a9716bb
Guan Xuetao <gxt@pku.edu.cn>
2022-05-19 17:55:55 +0800

    DADAO-bench: add embench framework
    Signed-off-by: Guan Xuetao <gxt@pku.edu.cn>

---
+++ DADAO-bench/embench-iot/config/dadao/boards/generic/boardsupport.c
@@ -0,0 +1,19 @@
+#include <support.h>
+
+void
+initialise_board ()
+{
+  __asm__ volatile ("swym" : : : "memory");
+}
+
+void __attribute__ ((noinline)) __attribute__ ((externally_visible))
+start_trigger ()
+{
+  __asm__ volatile ("swym" : : : "memory");
+}
+
+void __attribute__ ((noinline)) __attribute__ ((externally_visible))
+stop_trigger ()
+{
+  __asm__ volatile ("swym" : : : "memory");
+}
