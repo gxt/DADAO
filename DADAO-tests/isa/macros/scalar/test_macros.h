5fb45b9305fd7974eeaeed3d8182390fb9b4025a
Guan Xuetao <gxt@pku.edu.cn>
2022-10-29 21:25:01 +0800

    DADAO-tests: add isa/macros/scalar/test_macros.h
    Signed-off-by: Guan Xuetao <gxt@pku.edu.cn>

---
+++ DADAO-tests/isa/macros/scalar/test_macros.h
@@ -0,0 +1,47 @@
+#ifndef __TEST_MACROS_SCALAR_H
+#define __TEST_MACROS_SCALAR_H
+
+#-----------------------------------------------------------------------
+# Helper macros
+#-----------------------------------------------------------------------
+
+#define TEST_CASE( testnum, testreg, correctval, code... )	\
+test_ ## testnum:						\
+	move	TESTNUM, testnum;				\
+	code;							\
+	cmps	rd7, testreg, -1;				\
+	brnz	rd7, fail;
+
+#-----------------------------------------------------------------------
+# DADAO MACROS
+#-----------------------------------------------------------------------
+
+#-----------------------------------------------------------------------
+# Tests for an instruction with register-register operands
+#-----------------------------------------------------------------------
+
+#define TEST_ORRR_OP( testnum, inst, result, val1, val2 )	\
+    TEST_CASE( testnum, rd14, result,				\
+	move  rd1, val1;					\
+	move  rd2, val2;					\
+	inst  rd14, rd1, rd2;					\
+    )
+
+#-----------------------------------------------------------------------
+# Pass and fail code (assumes test num is in TESTNUM)
+#-----------------------------------------------------------------------
+
+#define TEST_PASSFAIL						\
+	brnz  TESTNUM, pass;					\
+fail:								\
+	DDTEST_FAIL;						\
+pass:								\
+	DDTEST_PASS;						\
+
+#-----------------------------------------------------------------------
+# Test data section
+#-----------------------------------------------------------------------
+
+#define TEST_DATA
+
+#endif /* __TEST_MACROS_SCALAR_H */
