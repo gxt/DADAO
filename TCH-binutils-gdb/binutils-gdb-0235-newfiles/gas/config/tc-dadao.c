5bca69ad5a06ed1c0d880f3b7b20a5fef8019986
zengxiaofeng <zengxiaofeng@pkunity.com>
2022-12-21 17:35:06 +0800

    TCH-bin: use swym(0x10000000) in dd_fill_nops.
    Signed-off-by: zengxiaofeng <zengxiaofeng@pkunity.com>

--- TCH-binutils-gdb/binutils-gdb-0235-newfiles/gas/config/tc-dadao.c
+++ TCH-binutils-gdb/binutils-gdb-0235-newfiles/gas/config/tc-dadao.c
@@ -200,7 +200,7 @@ static void dd_fill_nops(char *opcodep, int n)
     int i;
 
     for (i = 0; i < n; i++)
-        md_number_to_chars(opcodep + i * 4, 0 /* nop opcode is 0 */, 4);
+        md_number_to_chars(opcodep + i * 4, 0x10000000 /* nop opcode is swym */, 4);
 }
 
 /* Get up to four operands, filling them into the exp array.
