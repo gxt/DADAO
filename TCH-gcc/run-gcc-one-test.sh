89d4d9b0addfc756aa7fc40fdf3baab371902e03
Guan Xuetao <gxt@pku.edu.cn>
2023-02-03 15:27:50 +0800

    TCH-gcc: intro run-gcc-one-test.sh
    Signed-off-by: Guan Xuetao <gxt@pku.edu.cn>

---
+++ TCH-gcc/run-gcc-one-test.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo "0. Run this shell script with two parameters, for example: $0 ~/DADAO compile.exp zero-strct-5.c"
+echo "1. Check dir and change test file name BEFORE run this shell script"
+echo "2. gcc.sum and gcc.log will be overwritten AFTER run this shell script"
+
+export PATH=$1/__tests/dejagnu/usr/bin:/bin:/usr/bin
+make -C $1/__build/gcc-1003/ check-gcc-c RUNTESTFLAGS="--target_board=qemu-dadao $2=$3"
+
+echo "DONE"
