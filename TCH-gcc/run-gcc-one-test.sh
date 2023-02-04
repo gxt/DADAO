e16ce5fa422440f9c1e5c5e96fa7ab5bbd03be1a
Guan Xuetao <gxt@pku.edu.cn>
2023-02-04 10:28:57 +0800

    TCH-gcc: use qemu-dejagnu-prepare to replace tests-dejagnu-highfive
    Signed-off-by: Guan Xuetao <gxt@pku.edu.cn>

--- TCH-gcc/run-gcc-one-test.sh
+++ TCH-gcc/run-gcc-one-test.sh
@@ -3,7 +3,7 @@ echo "0. Run this shell script with two parameters, for example: $0 ~/DADAO comp
 echo "1. Check dir and change test file name BEFORE run this shell script"
 echo "2. gcc.sum and gcc.log will be overwritten AFTER run this shell script"
 
-export PATH=$1/__tests/dejagnu/usr/bin:/bin:/usr/bin
+export PATH=$1/__install/usr/bin:/bin:/usr/bin
 make -C $1/__build/gcc-1003/ check-gcc-c RUNTESTFLAGS="--target_board=qemu-dadao $2=$3"
 
 echo "DONE"
