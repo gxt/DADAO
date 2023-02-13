057f514f0aae4f9011af55a3b4961ea331a4b973
Guan Xuetao <gxt@pku.edu.cn>
2023-02-13 20:30:18 +0800

    ENV-linux: add arch/dadao/include/asm/atomic.h
    Signed-off-by: Guan Xuetao <gxt@pku.edu.cn>

---
+++ ENV-linux/linux-0504-newfiles/arch/dadao/include/asm/atomic.h
@@ -0,0 +1,8 @@
+#ifndef __ASM_DADAO_ATOMIC_H
+#define __ASM_DADAO_ATOMIC_H
+
+#include <asm/cmpxchg.h>
+#include <asm-generic/atomic.h>
+#include <asm-generic/atomic64.h>
+
+#endif /* __ASM_DADAO_ATOMIC_H */
