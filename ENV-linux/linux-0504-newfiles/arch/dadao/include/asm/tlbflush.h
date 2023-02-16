2058412c368f446a3fe6917fa346c3b54881d63a
Guan Xuetao <gxt@pku.edu.cn>
2023-02-16 10:32:16 +0800

    ENV-linux: add flush_tlb_range imp
    Signed-off-by: Guan Xuetao <gxt@pku.edu.cn>

--- ENV-linux/linux-0504-newfiles/arch/dadao/include/asm/tlbflush.h
+++ ENV-linux/linux-0504-newfiles/arch/dadao/include/asm/tlbflush.h
@@ -5,8 +5,18 @@
 #define flush_tlb_all()				BUG()
 #define flush_tlb_mm(mm)			BUG()
 #define flush_tlb_page(vma, addr)		BUG()
-#define flush_tlb_range(mm, start, end)		BUG()
 #define flush_tlb_pgtables(mm, start, end)	BUG()
 #define flush_tlb_kernel_range(start, end)	BUG()
+
+static inline void local_flush_tlb_all(void)
+{
+	__asm__ __volatile__ ("sfence.vma" : : : "memory");
+}
+
+static inline void flush_tlb_range(struct vm_area_struct *vma,
+		unsigned long start, unsigned long end)
+{
+	local_flush_tlb_all();
+}
 
 #endif /* __ASM_DADAO_TLBFLUSH_H */
