d24f34fae8580d48f4b2f3b8f9ca4a23068f5f49
Guan Xuetao <gxt@pku.edu.cn>
2023-02-23 10:31:21 +0800

    ENV-linux: fix const problem in arch/dadao/include/asm/arch_memory.h
    Signed-off-by: Guan Xuetao <gxt@pku.edu.cn>

--- ENV-linux/linux-0504-newfiles/arch/dadao/include/asm/arch_memory.h
+++ ENV-linux/linux-0504-newfiles/arch/dadao/include/asm/arch_memory.h
@@ -5,11 +5,13 @@
 #ifndef __DADAO_ASM_ARCH_MEMORY_H__
 #define __DADAO_ASM_ARCH_MEMORY_H__
 
+#include <linux/const.h>
+
 /* the whole 48-bit address space, 47-bit for user address space */
-#define __DD_MEMORY_TASK_SIZE		(0x800000000000UL)
+#define __DD_MEMORY_TASK_SIZE		ULL(0x800000000000)
 
 /* PAGE_OFFSET - the virtual address of the start of the kernel image */
-#define __DD_MEMORY_PAGE_OFFSET		UL(0xDADA00000000)
+#define __DD_MEMORY_PAGE_OFFSET		ULL(0xDADA00000000)
 #define __DD_MEMORY_VMALLOC_START	(__DD_MEMORY_PAGE_OFFSET + SZ_2G)
 #define __DD_MEMORY_VMALLOC_END		(__DD_MEMORY_PAGE_OFFSET + SZ_4G)
 
