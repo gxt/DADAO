d95ac5859f46baf152e53ac7ef461fb48680cd5d
Guan Xuetao <gxt@pku.edu.cn>
2023-02-28 14:59:44 +0800

    ENV-linux: add empty kernel/process.c
    Signed-off-by: Guan Xuetao <gxt@pku.edu.cn>

---
+++ ENV-linux/linux-0504-newfiles/arch/dadao/kernel/process.c
@@ -0,0 +1,17 @@
+#include <linux/cpu.h>
+#include <linux/kernel.h>
+#include <linux/sched.h>
+#include <linux/sched/task_stack.h>
+#include <linux/tick.h>
+#include <linux/ptrace.h>
+#include <linux/uaccess.h>
+
+void show_regs(struct pt_regs *regs)
+{
+}
+
+int copy_thread_tls(unsigned long clone_flags, unsigned long usp,
+	unsigned long arg, struct task_struct *p, unsigned long tls)
+{
+	return 0;
+}
