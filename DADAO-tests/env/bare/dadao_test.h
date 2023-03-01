de7746d26975f5e9225bcb35022bd9bafbc71d15
Guan Xuetao <gxt@pku.edu.cn>
2023-03-01 17:34:50 +0800

    DADAO-tests: rewrite DDTEST bare environment
    Signed-off-by: Guan Xuetao <gxt@pku.edu.cn>

--- DADAO-tests/env/bare/dadao_test.h
+++ DADAO-tests/env/bare/dadao_test.h
@@ -11,15 +11,15 @@
 	.macro init;							\
 	.endm
 
-#define INIT_DATAREG							\
+#define INIT_RD_RB_RF							\
 	rd2rd	rd1, rd0, 0;						\
 	rd2rd	rd2, rd0, 1;						\
 	rd2rd	rd4, rd0, 3;						\
 	rd2rd	rd8, rd0, 7;						\
 	rd2rd	rd16, rd0, 15;						\
-	rd2rd	rd32, rd0, 31;
-
-#define INIT_FLOATREG							\
+	rd2rd	rd32, rd0, 31;						\
+	rd2rb	rb1, rd1, 62;						\
+	rd2rf	rf1, rd1, 62;						\
 	setw	rf0, w3, 0x47ef;					\
 	setw	rf0, w2, 0xffff;					\
 	setw	rf0, w1, 0xe000;					\
@@ -33,51 +33,63 @@
 	.global _start;							\
 _start:									\
 	/* reset vector */						\
+	INIT_RD_RB_RF;							\
+	CPRD_mhartid(rd8);						\
+1:	brnz	rd8, 1b;						\
+	move	TESTNUM, 0;						\
 	move	rd8, trap_vector;					\
-	cpwr	cp0, cr12, cr5, rd8; /* mtvec */			\
-	jump	reset_vector;						\
-	.align 2;							\
+	CPWR_mtvec(rd8);						\
+	CPWR_mstatus(rd0);						\
+	init;								\
+	move	rd8, _test_start;					\
+	CPWR_mepc(rd8);							\
+	escape	cp0, 0; /* should goto _test_start */			\
 trap_vector:								\
 	/* test whether the test came from pass/fail */			\
-	move	rb32, 0x80001000;					\
-	stt	TESTNUM, rb32, 0;					\
-	stt	rd0, rb32, 4;						\
-__wait_fromhost:							\
-	ldtu	rd33, rb32, 0x40;	/* fromhost */			\
-	brz	rd33, __wait_fromhost;					\
-	stt	rd0, rb32, 0x40;					\
-	escape	cp0, 0;							\
-reset_vector:								\
-	INIT_DATAREG;							\
-	INIT_FLOATREG;							\
-	rd2rd	TESTNUM, rd0, 0;					\
-	init;								\
-	swym; /* TODO */
+	CPRD_mcause(rd8);						\
+	move	rd9, CAUSE_USER_ECALL;					\
+	breq	rd9, rd8, write_tohost;					\
+	move	rd9, CAUSE_SUPERVISOR_ECALL;				\
+	breq	rd9, rd8, write_tohost;					\
+	move	rd9, CAUSE_MACHINE_ECALL;				\
+	breq	rd9, rd8, write_tohost;					\
+	/* some unhandlable exception occurred */			\
+	orw	TESTNUM, w0, 1337;					\
+write_tohost:								\
+	move	rb8, tohost;						\
+	stt	TESTNUM, rb8, 0;					\
+	stt	rd0, rb8, 4;						\
+	jump	write_tohost;						\
+_test_start:
 
 //-----------------------------------------------------------------------
 // Pass/Fail Macro
 //-----------------------------------------------------------------------
 
 #define DDTEST_CODE_END							\
-	move	rd32, ((1337 << 1) | 1);				\
-	move	rb32, 0x80001000;					\
-	stt	rd32, rb32, 0;						\
-__tohost_exit:								\
-	jump	__tohost_exit;	/* noreturn */
+	unimp	0;							\
+	/* SHOULD cause illegal instruction */
 
 //-----------------------------------------------------------------------
 // Pass/Fail Macro
 //-----------------------------------------------------------------------
 
 #define DDTEST_PASS							\
+	fence	0;							\
 	move	TESTNUM, 1;						\
 	move	rd15, 93;						\
+	move	rd16, 0;						\
 	trap	cp0, 0;
 
 #define TESTNUM rd2
 #define DDTEST_FAIL							\
-	move rd15, 93;							\
-	trap cp0, 0;
+	fence	0;							\
+1:	breq	rd0, TESTNUM, 1b;					\
+	shlu	TESTNUM, TESTNUM, 1;					\
+	orw	TESTNUM, w0, 1;						\
+	move	rd15, 93;						\
+	rd2rd	rd16, TESTNUM, 0;					\
+	trap	cp0, 0;
 
 //-----------------------------------------------------------------------
 // Data Section Macro
