77d1ffb03887f9561a0b2da4fa5e7db390994b12
zengxiaofeng <zengxiaofeng@pkunity.com>
2023-03-02 18:05:29 +0800

    TCH-bin: fix the test of trap in gas.
    Signed-off-by: zengxiaofeng <zengxiaofeng@pkunity.com>

--- TCH-binutils-gdb/binutils-gdb-0235-newfiles/gas/testsuite/gas/dadao/insn-trap.d
+++ TCH-binutils-gdb/binutils-gdb-0235-newfiles/gas/testsuite/gas/dadao/insn-trap.d
@@ -5,7 +5,7 @@ tmpdir/insn-trap.o:     file format elf64-dadao
 Disassembly of section .text:
 
 0000000000000000 <_start>:
-   0:	7007ffff 	trap	cp1, 0x3ffff
+   0:	7607ffff 	trap	cp1, 0x3ffff
    4:	7107ffff 	cpco	cp1, 0x3ffff
    8:	72041081 	cprd	cp1, cr1, cr2, rd1
    c:	73041081 	cpwr	cp1, cr1, cr2, rd1
