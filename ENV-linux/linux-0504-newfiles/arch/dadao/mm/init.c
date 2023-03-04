691a7f6eb3845deeb127f8e0b09688f57dffc745
Guan Xuetao <gxt@pku.edu.cn>
2023-03-04 10:14:23 +0800

    ENV-linux: add swapper_pg_dir definition
    Signed-off-by: Guan Xuetao <gxt@pku.edu.cn>

--- ENV-linux/linux-0504-newfiles/arch/dadao/mm/init.c
+++ ENV-linux/linux-0504-newfiles/arch/dadao/mm/init.c
@@ -2,9 +2,12 @@
 #include <linux/init.h>
 #include <linux/linkage.h>
 #include <asm/page.h>
+#include <asm/pgtable.h>
 
 unsigned long empty_zero_page[PAGE_SIZE / sizeof(unsigned long)] __page_aligned_bss;
 EXPORT_SYMBOL(empty_zero_page);
+
+pgd_t swapper_pg_dir[PTRS_PER_PGD] __page_aligned_bss;
 
 void __init mem_init(void)
 {
