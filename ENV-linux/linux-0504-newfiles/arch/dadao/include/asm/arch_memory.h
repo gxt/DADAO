8b44cb090a974b43b8c1246a41d11b55538c9f53
Guan Xuetao <gxt@pku.edu.cn>
2023-03-04 09:00:37 +0800

    ENV-linux: shrink memory address to simple build processing
    Signed-off-by: Guan Xuetao <gxt@pku.edu.cn>

--- ENV-linux/linux-0504-newfiles/arch/dadao/include/asm/arch_memory.h
+++ ENV-linux/linux-0504-newfiles/arch/dadao/include/asm/arch_memory.h
@@ -8,10 +8,10 @@
 #include <linux/const.h>
 
 /* the whole 48-bit address space, 47-bit for user address space */
-#define __DD_MEMORY_TASK_SIZE		ULL(0x800000000000)
+#define __DD_MEMORY_TASK_SIZE		ULL(0x40000000)
 
 /* PAGE_OFFSET - the virtual address of the start of the kernel image */
-#define __DD_MEMORY_PAGE_OFFSET		ULL(0xDADA00000000)
+#define __DD_MEMORY_PAGE_OFFSET		ULL(0x80000000)
 #define __DD_MEMORY_VMALLOC_START	(__DD_MEMORY_PAGE_OFFSET + SZ_2G)
 #define __DD_MEMORY_VMALLOC_END		(__DD_MEMORY_PAGE_OFFSET + SZ_4G)
 
