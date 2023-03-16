4c9faaa3e23c0b21482fcf1777de33c3234b9970
zengxiaofeng <zengxiaofeng@pkunity.com>
2023-03-16 14:12:37 +0800

    TCH-bin: remove pseudo instruction--"move".
    Signed-off-by: zengxiaofeng <zengxiaofeng@pkunity.com>

--- TCH-binutils-gdb/binutils-gdb-0235-newfiles/gas/testsuite/gas/dadao/insn-pseudo.s
+++ TCH-binutils-gdb/binutils-gdb-0235-newfiles/gas/testsuite/gas/dadao/insn-pseudo.s
@@ -2,21 +2,20 @@
 	.text
 	.global	_start
 _start:
-	move	rb1, 0xffffffffffffffff
-	move	rb1, 0
+	setrb	rb1, 0xffffffffffffffff
+	setrb	rb1, 0
 
-	move	rd1, 0x0000000000001234
-	move	rd1, 0x00000000f00f1234
-	move	rd1, 0x0000f00f00001234
-	move	rd1, 0xf00f000000001234
-	move	rd1, 0xf00ff00f00001234
+	setrf	rf0, 0xffffffffffffffff
+	setrf	rf0, 0
 
-	move	rd1, 0xffff0000f00f1234
-	move	rd1, 0xfffff00f00001234
-	move	rd1, 0xf00fffff00001234
+	setrd	rd1, 0x0000000000001234
+	setrd	rd1, 0x00000000f00f1234
+	setrd	rd1, 0x0000f00f00001234
+	setrd	rd1, 0xf00f000000001234
+	setrd	rd1, 0xf00ff00f00001234
 
-	move	rd1, 0xffffffffffff1234
-	move	rd1, 0xfffffffff00f1234
-	move	rd1, 0xfffff00fffff1234
-	move	rd1, 0xf00fffffffff1234
-	move	rd1, 0xf00ff00fffff1234
+	setrd	rd1, 0xffff0000f00f1234
+	setrd	rd1, 0xfffffffff00f1234
+	setrd	rd1, 0xfffff00fffff1234
+	setrd	rd1, 0xf00fffffffff1234
+	setrd	rd1, 0xf00ff00fffff1234
--- TCH-binutils-gdb/binutils-gdb-0235-newfiles/gas/testsuite/gas/dadao/insn-set.s
+++ TCH-binutils-gdb/binutils-gdb-0235-newfiles/gas/testsuite/gas/dadao/insn-set.s
@@ -2,11 +2,11 @@
 	.text
 	.global _start
 _start:
-	move	rd1, 0xabcdabcd
-	move	rd2, 0x1357924680
-	move	rd4, 0x1234567812345678
-	move	rd5, 0
-	move	rd6, -1
-	move	rd7, 0xFFFFFFFE
-	move	rd8, 0xFFFF0000FFFF
-	move	rd9, 0xFFFF0000FFFF0000
+	setrd	rd1, 0xabcdabcd
+	setrd	rd2, 0x1357924680
+	setrd	rd4, 0x1234567812345678
+	setrd	rd5, 0
+	setrd	rd6, -1
+	setrd	rd7, 0xfffffffe
+	setrd	rd8, 0xffff0000ffff
+	setrd	rd9, 0xffff0000ffff0000
