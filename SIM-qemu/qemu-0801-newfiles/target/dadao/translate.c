6efd81a01bb36446b43bd3438cbb73e2b38a768a
Guan Xuetao <gxt@pku.edu.cn>
2024-04-02 15:56:31 +0800

    SIM-qemu: simple INSN_EXT_ORRI
    Signed-off-by: Guan Xuetao <gxt@pku.edu.cn>

--- SIM-qemu/qemu-0801-newfiles/target/dadao/translate.c
+++ SIM-qemu/qemu-0801-newfiles/target/dadao/translate.c
@@ -588,25 +588,19 @@ INSN_LOGIC_ORRI(SHRSi, sari)
 
 #undef INSN_LOGIC_ORRI
 
-static bool trans_EXTSi(DisasContext *ctx, arg_EXTSi *a)
-{
-    if (a->hb == 0) {
-        return true;
-    }
-    tcg_gen_shli_i64(cpu_rd[a->hb], cpu_rd[a->hc], a->immu6);
-    tcg_gen_sari_i64(cpu_rd[a->hb], cpu_rd[a->hb], a->immu6);
-    return true;
-}
+#define INSN_EXT_ORRI(insn, op)															\
+	static bool trans_##insn(DisasContext *ctx, arg_##insn *a)							\
+	{																					\
+		if (a->hb == 0)			return true;											\
+		tcg_gen_shli_i64(cpu_rd[a->hb], cpu_rd[a->hc], a->immu6);						\
+		tcg_gen_##op##_i64(cpu_rd[a->hb], cpu_rd[a->hb], a->immu6);						\
+		return true;																	\
+	}
 
-static bool trans_EXTZi(DisasContext *ctx, arg_EXTZi *a)
-{
-    if (a->hb == 0) {
-        return true;
-    }
-    tcg_gen_shli_i64(cpu_rd[a->hb], cpu_rd[a->hc], a->immu6);
-    tcg_gen_shri_i64(cpu_rd[a->hb], cpu_rd[a->hb], a->immu6);
-    return true;
-}
+INSN_EXT_ORRI(EXTSi, sari)
+INSN_EXT_ORRI(EXTZi, shri)
+
+#undef INSN_EXT_ORRI
 
 static bool trans_fcvt_all(DisasContext *ctx, arg_disas_dadao2 *a,
                            TCGv_i64* cpu_hb, TCGv_i64* cpu_hc,
