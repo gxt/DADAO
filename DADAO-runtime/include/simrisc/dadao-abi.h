aeecc1aea1ffdb39d831228f2d9f822477d54941
Guan Xuetao <gxt@pku.edu.cn>
2024-04-27 20:10:02 +0800

    DADAO-runtime: add dadao-abi.h
    Signed-off-by: Guan Xuetao <gxt@pku.edu.cn>

---
+++ DADAO-runtime/include/simrisc/dadao-abi.h
@@ -0,0 +1,19 @@
+#ifndef __DADAO_ABI_H__
+#define __DADAO_ABI_H__
+
+#define __DDABI_RDZERO_NR__								0
+#define __DDABI_RDERRNO_NR__							1
+
+#define __DDABI_RBIP_NR__								0
+#define __DDABI_RBSP_NR__								1
+#define __DDABI_RBFP_NR__								2
+#define __DDABI_RBGP_NR__								3
+#define __DDABI_RBTP_NR__								4
+
+#define __DDABI_RFCSR_NR__								0
+#define __DDABI_RFTEPS_NR__								0
+#define __DDABI_RFOEPS_NR__								1
+
+#define __DDABI_RASP_NR__								63
+
+#endif
