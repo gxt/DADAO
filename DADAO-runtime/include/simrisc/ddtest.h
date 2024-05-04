c1b99b18a36331b54b3ead9fab0a95c0650a9361
Guan Xuetao <gxt@pku.edu.cn>
2024-05-04 15:03:13 +0800

    DADAO-runtime: add NaN defs
    Signed-off-by: Guan Xuetao <gxt@pku.edu.cn>

--- DADAO-runtime/include/simrisc/ddtest.h
+++ DADAO-runtime/include/simrisc/ddtest.h
@@ -19,10 +19,15 @@
 
 #define RF_FCSR				rf0
 
-#define ft_qNaN				__DDAEE_FLOAT_TETRA_QNAN__
-#define ft_sNaN				__DDAEE_FLOAT_TETRA_SNAN__
-#define fo_qNaN				__DDAEE_FLOAT_OCTA_QNAN__
-#define fo_sNaN				__DDAEE_FLOAT_OCTA_SNAN__
+#define ft_qNaN				0f:7fc00000
+#define ft_sNaN				0f:7f800001
+#define fo_qNaN				0d:7ff8000000000000
+#define fo_sNaN				0d:7ff0000000000001
+
+#define it_qNaN				__DDAEE_FLOAT_TETRA_QNAN__
+#define it_sNaN				__DDAEE_FLOAT_TETRA_SNAN__
+#define io_qNaN				__DDAEE_FLOAT_OCTA_QNAN__
+#define io_sNaN				__DDAEE_FLOAT_OCTA_SNAN__
 
 #define fflags_Z			0
 #define fflag_NV			__DDAEE_FCSR_FFLAG_INVALID__
