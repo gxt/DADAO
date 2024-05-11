38312ca44602ccf1653a78da75dce9212c9c988e
Guan Xuetao <gxt@pku.edu.cn>
2024-05-11 17:01:43 +0800

    DADAO-testset: adjust order in ddrf
    Signed-off-by: Guan Xuetao <gxt@pku.edu.cn>

--- DADAO-testset/isa/ddrf/ddrf.mk
+++ DADAO-testset/isa/ddrf/ddrf.mk
@@ -3,16 +3,16 @@
 #-----------------------------------------------------------------------
 
 ddrf_sc_tests = \
-	rwii-setw \
-	orri-fx2ix orri-fx2ux orri-ix2fx orri-ux2fx orri-fx2fx \
-	orri-fxclass \
-	orri-fxsqrt \
-	orri-rx2rx \
-	orrr-fxadd orrr-fxsub orrr-fxmul orrr-fxdiv \
-	orrr-fxqcmp orrr-fxscmp \
-	orrr-fxsgnj orrr-fxsgnjn \
 	rrii-ldfx \
+	orri-rx2rx \
+	rwii-setw \
+	orri-fx2fx orri-fx2ix orri-fx2ux orri-ix2fx orri-ux2fx \
+	orrr-fxadd orrr-fxsub orrr-fxmul orrr-fxdiv \
 	rrrr-fxmadd \
+	orri-fxsqrt \
+	orrr-fxsgnj orrr-fxsgnjn \
+	orrr-fxqcmp orrr-fxscmp \
+	orri-fxclass \
 
 ddrf_bare_tests = $(addprefix ddrf-bare-, $(ddrf_sc_tests))
 ddrf_qemu_tests = $(addprefix ddrf-qemu-, $(ddrf_sc_tests))
