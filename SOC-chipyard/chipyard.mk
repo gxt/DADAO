fd7ac1390810c706a2a69efbd49a7ae7f8318757
Guan Xuetao <gxt@pku.edu.cn>
2024-06-05 17:39:58 +0800

    TCH-newlib: use __VAR_L__
    Signed-off-by: Guan Xuetao <gxt@pku.edu.cn>

--- SOC-chipyard/chipyard.mk
+++ SOC-chipyard/chipyard.mk
@@ -31,7 +31,7 @@ chipyard-0107-update-instructions:
 chipyard-0107-source:
 	@rm -fr $(CHIPYARD_0107_SOURCE)
 	# Clone github repo
-	@git clone -q $(CHIPYARD_0107_GITHUB) -- $(CHIPYARD_0107_SOURCE)
+	@$(__VAR_L__) git clone -q $(CHIPYARD_0107_GITHUB) -- $(CHIPYARD_0107_SOURCE)
 	@cd $(CHIPYARD_0107_SOURCE); git checkout -qb $(CHIPYARD_0107_BRANCH) $(CHIPYARD_0107_VERSION)
 	# Setup submodules by terribly downloading git repos from github.com
 	@cd $(CHIPYARD_0107_SOURCE); ./scripts/init-submodules-no-riscv-tools.sh
