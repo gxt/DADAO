fe022328dcb4a65fb1cd915d86e3619cb2b6348d
Guan Xuetao <gxt@pku.edu.cn>
2024-06-12 15:21:06 +0800

    SOC-chipyard: Bump WumingConfigs.scala to latest rocket-chip/chisel3.5.6 and testchipip for more organized package
    Signed-off-by: Guan Xuetao <gxt@pku.edu.cn>

--- SOC-chipyard/chipyard-0111-newfiles/generators/chipyard/src/main/scala/config/WumingConfigs.scala
+++ SOC-chipyard/chipyard-0111-newfiles/generators/chipyard/src/main/scala/config/WumingConfigs.scala
@@ -2,13 +2,13 @@ package chipyard
 
 import chisel3._
 
-import freechips.rocketchip.config.{Config}
+import org.chipsalliance.cde.config.{Config}
 
 class Wuming1StageConfig extends Config(
   // Create a Wuming 1-stage core
   new wuming.common.WithNWumingCores(1, internalTile = wuming.common.Stage1Factory) ++
-  new testchipip.WithSerialTLWidth(32) ++
-  new testchipip.WithSerialPBusMem ++
+  new testchipip.soc.WithNoScratchpads ++                      // No scratchpads
+  new testchipip.serdes.WithSerialTLWidth(32) ++
   new freechips.rocketchip.subsystem.WithScratchpadsOnly ++    // use wuming tile-internal scratchpad
   new freechips.rocketchip.subsystem.WithNoMemPort ++          // use no external memory
   new freechips.rocketchip.subsystem.WithNBanks(0) ++
@@ -17,8 +17,8 @@ class Wuming1StageConfig extends Config(
 class Wuming5StageConfig extends Config(
   // Create a Wuming 5-stage core
   new wuming.common.WithNWumingCores(1, internalTile = wuming.common.Stage5Factory) ++
-  new testchipip.WithSerialTLWidth(32) ++
-  new testchipip.WithSerialPBusMem ++
+  new testchipip.soc.WithNoScratchpads ++                      // No scratchpads
+  new testchipip.serdes.WithSerialTLWidth(32) ++
   new freechips.rocketchip.subsystem.WithScratchpadsOnly ++    // use wuming tile-internal scratchpad
   new freechips.rocketchip.subsystem.WithNoMemPort ++          // use no external memory
   new freechips.rocketchip.subsystem.WithNBanks(0) ++
