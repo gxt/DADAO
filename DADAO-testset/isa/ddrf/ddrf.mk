f9bde0481668ffab325db8d8c5d8c4cc791c6232
Guan Xuetao <gxt@pku.edu.cn>
2024-06-21 10:39:22 +0800

    DADAO-testset: add orri-fxlog test cases
    Signed-off-by: Guan Xuetao <gxt@pku.edu.cn>

--- DADAO-testset/isa/ddrf/ddrf.mk
+++ DADAO-testset/isa/ddrf/ddrf.mk
@@ -8,7 +8,7 @@ ddrf_passqemu_tests =		\
 	orri-fx2fx	orri-fx2ix	orri-fx2ux	orri-ix2fx	orri-ux2fx	\
 	orrr-fxadd	orrr-fxsub	orrr-fxmul	orrr-fxdiv	orrr-fxrem	\
 	rrrr-fxmadd				\
-	orri-fxroot				\
+	orri-fxroot	orri-fxlog	\
 	orrr-fxsgnj	orrr-fxsgnn	\
 	orrr-fxqcmp	orrr-fxscmp	\
 	rrrr-csx	orrr-csxx	\
